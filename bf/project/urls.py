8d74e10fed732003b7ddd637f8cb71cb925c8d9f
Lars Peter Thomsen <larsp@magenta.dk>
2024-05-06 14:07:08 +0200

    feat: [#60742] Pipeline

--- bf/project/urls.py
+++ bf/project/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
+    path(
+        "",
+        include(
+            "bf.urls",
+            namespace="bf",
+        ),
+    ),
 ]
