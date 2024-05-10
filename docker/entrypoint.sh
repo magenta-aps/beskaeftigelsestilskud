83b0d0648bf5c3166b9b402cb8d350f1f8103036
Lars Peter Thomsen <larsp@magenta.dk>
2024-05-10 12:20:03 +0200

    feat: [#60742] Django setup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -35,13 +35,11 @@ if [ "${MAKEMESSAGES,,}" = true ]; then
   echo 'making messages'
   python manage.py makemessages --locale=kl --no-obsolete --add-location file
   python manage.py makemessages --locale=da --no-obsolete --add-location file
-  python manage.py makemessages --locale=en --no-obsolete --add-location file
 fi
 if [ "${COMPILEMESSAGES,,}" = true ]; then
   echo 'compiling messages'
   python manage.py compilemessages --locale=kl
   python manage.py compilemessages --locale=da
-  python manage.py compilemessages --locale=en
 fi
 
 exec "$@"
