c21483119abea65518d455a4b52e3e6a4ccb8b22
Mads Riis-Weitling <mrw@magenta.dk>
2024-06-04 09:38:42 +0200

    feat: [#61050] Add `PersonMonth` model in `bf` app

---
+++ bf/bf/models.py
@@ -0,0 +1,56 @@
+from datetime import date
+
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from eskat.models import ESkatMandtal
+
+
+class PersonMonth(models.Model):
+    class Meta:
+        ordering = ["cpr"]
+        unique_together = ["cpr", "date"]
+        index_together = ["cpr", "date"]
+
+    date = models.DateField(
+        null=False,
+        blank=False,
+        verbose_name=_("Dato"),
+    )
+
+    cpr = models.TextField(
+        null=False,
+        blank=False,
+        max_length=10,
+        validators=(RegexValidator(regex=r"\d{10}"),),
+        verbose_name=_("CPR nummer"),
+        help_text=_("CPR nummer"),
+
+    )
+
+    name = models.TextField(blank=True, null=True)
+    municipality_code = models.IntegerField(blank=True, null=True)
+    municipality_name = models.TextField(blank=True, null=True)
+    address_line_1 = models.TextField(blank=True, null=True)
+    address_line_2 = models.TextField(blank=True, null=True)
+    address_line_3 = models.TextField(blank=True, null=True)
+    address_line_4 = models.TextField(blank=True, null=True)
+    address_line_5 = models.TextField(blank=True, null=True)
+    full_address = models.TextField(blank=True, null=True)
+
+    @classmethod
+    def from_eskat_mandtal(cls, eskat_mandtal: ESkatMandtal, date: date) -> "PersonMonth":
+        return PersonMonth(
+            date=date,
+            cpr=eskat_mandtal.cpr,
+            name=eskat_mandtal.navn,
+            municipality_code=eskat_mandtal.kommune_no,
+            municipality_name=eskat_mandtal.kommune,
+            address_line_1=eskat_mandtal.adresselinje1,
+            address_line_2=eskat_mandtal.adresselinje2,
+            address_line_3=eskat_mandtal.adresselinje3,
+            address_line_4=eskat_mandtal.adresselinje4,
+            address_line_5=eskat_mandtal.adresselinje5,
+            full_address=eskat_mandtal.fuld_adresse,
+        )
