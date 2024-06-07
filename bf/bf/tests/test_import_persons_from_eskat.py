60add799bdeb74d11a7e5a8d9d71fb3c5284ceb3
Mads Riis-Weitling <mrw@magenta.dk>
2024-06-07 12:20:10 +0200

    fix: [#61050] Update `import_persons_from_eskat` and tests
    The management command now performs "create or update" logic for
    `Person` and `PersonMonth` objects.
    
    As we can no longer use `.bulk_create(update_conflicts=True, ...)`, the
    code is somewhat more involved.

--- bf/bf/tests/test_import_persons_from_eskat.py
+++ bf/bf/tests/test_import_persons_from_eskat.py
@@ -1,70 +1,125 @@
 import os
 from csv import reader
 from datetime import date
+from io import StringIO
 from uuid import uuid4
 
+from django.core.management import call_command
 from django.db import connections
 from django.test import TestCase
-from eskat.models import ESkatMandtal
 
 from bf.management.commands.import_persons_from_eskat import Command
-from bf.models import PersonMonth
+from bf.models import Person, PersonMonth
 
 
 class TestImportPersonsFromESkat(TestCase):
     databases = ["default", "eskat"]
+
+    _expected_mandtal_cpr = "0101012222"
+    _import_date = date(2020, 1, 1)
 
     def setUp(self):
         super().setUp()
         self._prepare_mock_eskat_db()
         self.command = Command()
 
-    def test_creates_person_month_objects(self):
-        # Load data for January 2020
+    def test_create_new_person_from_mandtal(self):
+        # Assert that we start from an empty state
+        self._assert_empty_state()
+
+        # Act: load data
         self._load_mock_eskat_db_fixture("mandtal.csv")
-        self.command.handle(import_date="2020-01-01")
-        # Assert that objects have been created for each CPR
-        self.assertQuerySetEqual(
-            PersonMonth.objects.filter(import_date=date(2020, 1, 1)).values_list(
-                "cpr", flat=True
-            ),
-            ESkatMandtal.objects.values_list("cpr", flat=True),
+        output = self._call_command(self._import_date)
+
+        # Assert that a new `Person` and `PersonMonth` is created
+        self._assert_person_exists_with_name("Original Name")
+        self._assert_person_month_exists(self._import_date)
+
+        # Assert output matches expectation
+        self.assertIn(
+            "Created 1 new persons, updated 0 persons, "
+            "created 1 new person months, and updated 0 person months.",
+            output,
         )
 
     def test_updates_person_month_on_change(self):
-        # Load original data for January 2020
+        # Assert that we start from an empty state
+        self._assert_empty_state()
+
+        # 1. Load original data
         self._load_mock_eskat_db_fixture("mandtal.csv")
-        self.command.handle(import_date="2020-01-01")
-        # Assert original CPR data is imported
-        self.assertQuerySetEqual(
-            PersonMonth.objects.filter(
-                import_date=date(2020, 1, 1),
-                cpr="0101012222",
-            ).values_list("name", flat=True),
-            ["Original Name"],
+        output_1 = self._call_command(self._import_date)
+        # 1. Assert original CPR data is imported
+        self._assert_person_exists_with_name("Original Name")
+        self._assert_person_month_exists(self._import_date, municipality_code=101)
+        # 1. Assert output matches expectation
+        self.assertIn(
+            "Created 1 new persons, updated 0 persons, "
+            "created 1 new person months, and updated 0 person months.",
+            output_1,
         )
 
-        # Load updated data for January 2020
+        # 2. Load updated data
         self._load_mock_eskat_db_fixture("mandtal_updated.csv")
-        self.command.handle(import_date="2020-01-01")
-        # Assert person month object is updated
-        self.assertQuerySetEqual(
-            PersonMonth.objects.filter(
-                import_date=date(2020, 1, 1),
-                cpr="0101012222",
-            ).values_list("name", flat=True),
-            ["Updated Name"],
+        output_2 = self._call_command(self._import_date)
+        # 2. Assert `Person` object is updated
+        self._assert_person_exists_with_name("Updated Name")
+        # 2. Assert `PersonMonth` object is not duplicated (due to two imports for the
+        # same import date.) Assert `PersonMonth` is updated (the `municipality_code`
+        # is updated.)
+        self._assert_person_month_exists(self._import_date, municipality_code=102)
+        # 2. Assert output matches expectation
+        self.assertIn(
+            "Created 0 new persons, updated 1 persons, "
+            "created 0 new person months, and updated 1 person months.",
+            output_2,
         )
 
     def test_creates_no_person_month_objects(self):
-        # Load data for February 2020 (empty set)
+        # Assert that we start from an empty state:
+        self._assert_empty_state()
+
+        # Load empty dataset
         self._load_mock_eskat_db_fixture("mandtal_empty.csv")
-        self.command.handle(import_date="2020-02-01")
-        # Assert that no person months exist for February
-        self.assertQuerySetEqual(
-            PersonMonth.objects.filter(import_date=date(2020, 2, 1)),
-            PersonMonth.objects.none(),
+        output = self._call_command(self._import_date)
+
+        # Assert that no `Person` or `PersonMonth` objects have been created or updated
+        self._assert_empty_state()
+        self.assertIn(
+            "Created 0 new persons, updated 0 persons, "
+            "created 0 new person months, and updated 0 person months",
+            output,
         )
+
+    def _assert_empty_state(self):
+        # Assert `Person` and `PersonMonth` data does not exist (yet)
+        self.assertQuerySetEqual(Person.objects.all(), Person.objects.none())
+        self.assertQuerySetEqual(PersonMonth.objects.all(), PersonMonth.objects.none())
+
+    def _assert_person_exists_with_name(self, name: str):
+        qs = Person.objects.filter(cpr=self._expected_mandtal_cpr).values_list(
+            "cpr", "name"
+        )
+        self.assertQuerySetEqual(qs, [(self._expected_mandtal_cpr, name)])
+
+    def _assert_person_month_exists(self, import_date: date, **field_values):
+        self.assertQuerySetEqual(
+            PersonMonth.objects.filter(import_date=import_date).values_list(
+                "person__cpr",
+                *field_values.keys(),
+            ),
+            [(self._expected_mandtal_cpr, *field_values.values())],
+        )
+
+    def _call_command(self, import_date: date) -> str:
+        buf = StringIO()
+        call_command(
+            self.command,
+            import_date.strftime("%Y-%m-%d"),
+            stdout=buf,
+            stderr=buf,
+        )
+        return buf.getvalue()
 
     def _prepare_mock_eskat_db(self):
         mock_eskat_db = connections["eskat"]
