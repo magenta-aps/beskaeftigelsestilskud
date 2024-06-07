e8f8ad706fdc5d689c71ed9fc60f70b12ea42ff3
Mads Riis-Weitling <mrw@magenta.dk>
2024-06-07 12:13:57 +0200

    fix: [#61050] Break `PersonMonth` into `Person` and `PersonMonth`
    `Person` tracks person data whose changes do not affect tax
    calculations.
    
    `PersonMonth` tracks monthly changes per person which affect tax
    calculations.
    
    `Person` uses `django-simple-history` to keep a history of changes.

--- bf/bf/models.py
+++ bf/bf/models.py
@@ -4,18 +4,13 @@ from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from eskat.models import ESkatMandtal
+from simple_history.models import HistoricalRecords
 
 
-class PersonMonth(models.Model):
-    class Meta:
-        ordering = ["cpr"]
-        unique_together = ["cpr", "import_date"]
-        index_together = ["cpr", "import_date"]
-
-    import_date = models.DateField(
-        null=False,
-        blank=False,
-        verbose_name=_("Dato"),
+class Person(models.Model):
+    history = HistoricalRecords(
+        history_change_reason_field=models.TextField(null=True),
+        related_name="history_entries",
     )
 
     cpr = models.TextField(
@@ -28,31 +23,54 @@ class PersonMonth(models.Model):
     )
 
     name = models.TextField(blank=True, null=True)
-    municipality_code = models.IntegerField(blank=True, null=True)
-    municipality_name = models.TextField(blank=True, null=True)
     address_line_1 = models.TextField(blank=True, null=True)
     address_line_2 = models.TextField(blank=True, null=True)
     address_line_3 = models.TextField(blank=True, null=True)
     address_line_4 = models.TextField(blank=True, null=True)
     address_line_5 = models.TextField(blank=True, null=True)
     full_address = models.TextField(blank=True, null=True)
-    fully_tax_liable = models.BooleanField(blank=True, null=True)
 
     @classmethod
-    def from_eskat_mandtal(
-        cls, eskat_mandtal: ESkatMandtal, import_date: date
-    ) -> "PersonMonth":
-        return PersonMonth(
-            import_date=import_date,
+    def from_eskat_mandtal(cls, eskat_mandtal: ESkatMandtal) -> "Person":
+        return Person(
             cpr=eskat_mandtal.cpr,
             name=eskat_mandtal.navn,
-            municipality_code=eskat_mandtal.kommune_no,
-            municipality_name=eskat_mandtal.kommune,
             address_line_1=eskat_mandtal.adresselinje1,
             address_line_2=eskat_mandtal.adresselinje2,
             address_line_3=eskat_mandtal.adresselinje3,
             address_line_4=eskat_mandtal.adresselinje4,
             address_line_5=eskat_mandtal.adresselinje5,
             full_address=eskat_mandtal.fuld_adresse,
+        )
+
+
+class PersonMonth(models.Model):
+    person = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
+    )
+
+    import_date = models.DateField(
+        null=False,
+        blank=False,
+        verbose_name=_("Dato"),
+    )
+
+    municipality_code = models.IntegerField(blank=True, null=True)
+    municipality_name = models.TextField(blank=True, null=True)
+    fully_tax_liable = models.BooleanField(blank=True, null=True)
+
+    @classmethod
+    def from_eskat_mandtal(
+        cls,
+        eskat_mandtal: ESkatMandtal,
+        person: Person,
+        import_date: date,
+    ) -> "PersonMonth":
+        return PersonMonth(
+            person=person,
+            import_date=import_date,
+            municipality_code=eskat_mandtal.kommune_no,
+            municipality_name=eskat_mandtal.kommune,
             fully_tax_liable=eskat_mandtal.fully_tax_liable,
         )
