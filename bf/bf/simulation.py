33e09d4ee122d07895685c679624e15050b86184
Mads Riis-Weitling <mrw@magenta.dk>
2024-06-14 16:42:47 +0200

    chore: [#61082] Simplify calculation logic in `Simulation`
    Now, it only considers one `Person` for each `Simulation`.
    Each simulation output consists of one row per `Employer`.
    Each row contains one or more prediction data series, depending on the
    `engines` argument.

--- bf/bf/simulation.py
+++ bf/bf/simulation.py
@@ -2,13 +2,9 @@ from dataclasses import dataclass
 from datetime import date
 from decimal import Decimal
 
-from django.db.models import F, Q, QuerySet, Sum
+from django.db.models import F, Q, Sum
 
-from bf.calculate import (
-    CalculationEngine,
-    InYearExtrapolationEngine,
-    TwelveMonthsSummationEngine,
-)
+from bf.calculate import CalculationEngine
 from bf.models import ASalaryReport, Employer, Person
 
 
@@ -21,20 +17,25 @@ class IncomeItem:
 
 @dataclass(frozen=True)
 class PredictionItem:
+    year: int
     month: int
     predicted_value: int
     prediction_difference: Decimal
     prediction_difference_pct: Decimal
 
 
+@dataclass(frozen=True)
+class Prediction:
+    engine: CalculationEngine
+    items: list[PredictionItem]
+
+
 @dataclass(frozen=True, repr=False)
 class SimulationResultRow:
-    person: Person
     employer: Employer
     income_series: list[IncomeItem]
     income_sum: int
-    engine: CalculationEngine
-    predictions: list[PredictionItem]
+    predictions: list[Prediction]
 
 
 @dataclass(frozen=True)
@@ -46,73 +47,67 @@ class Simulation:
     def __init__(
         self,
         engines: list[CalculationEngine],
-        person_qs: QuerySet[Person] = None,
+        person: Person,
         year: int = date.today().year,
     ):
-        self._person_qs = person_qs or Person.objects.all()
-        self._engines = engines
-        self._year = year
+        self.engines = engines
+        self.person = person
+        self.year = year
         self.result = SimulationResult(rows=list(self._run()))
 
     def _run(self):
-        for person in self._person_qs:
-            qs = ASalaryReport.objects.alias(
-                person=F("person_month__person_year__person"),
-                year=F("person_month__person_year__year"),
-                month=F("person_month__month"),
-            ).filter(person=person.pk)
-            employers = [x.employer for x in qs.distinct("employer")]
-            for employer in employers:
-                employment = qs.filter(employer=employer)
-                actual_year_sum = employment.filter(year=self._year).aggregate(
-                    s=Sum("amount")
-                )["s"]
+        qs = ASalaryReport.objects.alias(
+            person=F("person_month__person_year__person"),
+            year=F("person_month__person_year__year"),
+            month=F("person_month__month"),
+        ).filter(person=self.person.pk)
+        employers = [x.employer for x in qs.distinct("employer")]
+        for employer in employers:
+            employment = qs.filter(employer=employer)
+            actual_year_sum = employment.filter(year=self.year).aggregate(
+                s=Sum("amount")
+            )["s"]
 
-                income_series = [
-                    IncomeItem(year=item.year, month=item.month, value=item.amount)
-                    for item in employment
-                ]
+            income_series = [
+                IncomeItem(year=item.year, month=item.month, value=item.amount)
+                for item in employment
+            ]
 
-                for engine in self._engines:
-                    predictions = []
-                    for month in range(1, 13):
-                        visible_datapoints = employment.filter(
-                            Q(year__lt=self._year)
-                            | Q(year=self._year, month__lte=month)
-                        )
-                        resultat = engine.calculate(visible_datapoints)
-                        predictions.append(
-                            PredictionItem(
-                                month=month,
-                                predicted_value=resultat.year_prediction,
-                                prediction_difference=resultat.year_prediction
-                                - actual_year_sum,
-                                prediction_difference_pct=(
-                                    (
-                                        abs(
-                                            (resultat.year_prediction - actual_year_sum)
-                                            / actual_year_sum
-                                        )
-                                        * 100
+            predictions = []
+
+            for engine in self.engines:
+                prediction_items = []
+                for month in range(1, 13):
+                    visible_datapoints = employment.filter(
+                        Q(year__lt=self.year) | Q(year=self.year, month__lte=month)
+                    )
+                    resultat = engine.calculate(visible_datapoints)
+                    prediction_items.append(
+                        PredictionItem(
+                            year=self.year,
+                            month=month,
+                            predicted_value=resultat.year_prediction,
+                            prediction_difference=resultat.year_prediction
+                            - actual_year_sum,
+                            prediction_difference_pct=(
+                                (
+                                    abs(
+                                        (resultat.year_prediction - actual_year_sum)
+                                        / actual_year_sum
                                     )
-                                    if actual_year_sum != 0
-                                    else None
-                                ),
-                            )
+                                    * 100
+                                )
+                                if actual_year_sum != 0
+                                else None
+                            ),
                         )
-
-                    yield SimulationResultRow(
-                        person=person,
-                        employer=employer,
-                        income_series=income_series,
-                        income_sum=actual_year_sum,
-                        engine=engine,
-                        predictions=predictions,
                     )
 
+                predictions.append(Prediction(engine=engine, items=prediction_items))
 
-sim = Simulation(
-    [InYearExtrapolationEngine(), TwelveMonthsSummationEngine()],
-    Person.objects.all(),
-    year=2024,
-)
+            yield SimulationResultRow(
+                employer=employer,
+                income_series=income_series,
+                income_sum=actual_year_sum,
+                predictions=predictions,
+            )
