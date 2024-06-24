6da3d27881de35a754225396a477b23811af68b1
Mads Riis-Weitling <mrw@magenta.dk>
2024-06-24 09:40:07 +0200

    feat: [#61082] `load_csv`: support new dataset; use bulk inserts
    This alters the code to support loading data from additional columns in
    `2022_a.csv` and `2023_a.csv` ("indhandling" data per month.)
    
    This alters the code to use bulk inserts where possible, which requires
    some new constraints on some models:
    - `Person.cpr` must be unique
    - `Employer.cpr` must be unique
    - `PersonMonth`: `person_year` and `month` must be unique together.
    Loading the new dataset file `2022_a.csv` previously took ~66 minutes,
    and now takes ~7 minutes.

--- bf/bf/management/commands/load_csv.py
+++ bf/bf/management/commands/load_csv.py
@@ -8,6 +8,7 @@ from datetime import date
 from typing import List
 
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from bf.models import ASalaryReport, Employer, Person, PersonMonth, PersonYear
 
@@ -33,21 +34,19 @@ class IndkomstCSVFileLine:
                 cpr=int(list_get(row, 0)),
                 arbejdsgiver=list_get(row, 1),
                 cvr=int(list_get(row, 2)),
-                amounts=[
-                    list_get(row, 3) or 0,
-                    list_get(row, 4) or 0,
-                    list_get(row, 5) or 0,
-                    list_get(row, 6) or 0,
-                    list_get(row, 7) or 0,
-                    list_get(row, 8) or 0,
-                    list_get(row, 9) or 0,
-                    list_get(row, 10) or 0,
-                    list_get(row, 11) or 0,
-                    list_get(row, 12) or 0,
-                    list_get(row, 13) or 0,
-                    list_get(row, 14) or 0,
-                ],
+                amounts=cls._get_columns(row, 3, 3 + 24),
             )
+
+    @classmethod
+    def _get_column(cls, row, index: int, default: int = 0) -> int:
+        try:
+            return int(row[index]) or default
+        except (ValueError, IndexError):
+            return default
+
+    @classmethod
+    def _get_columns(cls, row, start: int, end: int) -> list[int]:
+        return [cls._get_column(row, index) for index in range(start, end)]
 
 
 class Command(BaseCommand):
@@ -55,44 +54,103 @@ class Command(BaseCommand):
         parser.add_argument("file", type=str)
         parser.add_argument("--count", type=int)
         parser.add_argument("--year", type=int)
+        parser.add_argument("--delimiter", type=str, default=";")
+        parser.add_argument("--dry", action="store_true")
 
     def handle(self, *args, **kwargs):
         with open(kwargs["file"]) as input_stream:
-            count = kwargs.get("count")
-            year = kwargs.get("year") or date.today().year
-            self._read_csv(input_stream, year, count)
+            self._year = kwargs.get("year") or date.today().year
+            self._delimiter = kwargs["delimiter"]
+            dry = kwargs["dry"]
+            rows = self._read_csv(input_stream, kwargs["count"])
+            if dry:
+                for row in rows:
+                    print(row)
+            else:
+                self._create_or_update_objects(rows)
 
-    def _read_csv(self, input_stream, year, count=None):
-        reader = csv.reader(input_stream, delimiter=";")
+    def _read_csv(self, input_stream, count=None):
+        reader = csv.reader(input_stream, delimiter=self._delimiter)
 
         next(reader)  # skip csv header
 
         for i, row in enumerate(reader):
-            if count is not None and i > count:
+            if count is not None and i >= count:
                 break
             # We are not yet at last line in file. Parse it as a regular item
             line = IndkomstCSVFileLine.from_csv_row(row)
             if line:
-                person, _ = Person.objects.get_or_create(
-                    name=line.cpr, cpr=line.cpr, defaults={}
+                yield line
+
+    @transaction.atomic
+    def _create_or_update_objects(self, rows):
+        rows = list(rows)
+
+        # Create or update Person objects
+        persons = {
+            cpr: Person(cpr=cpr, name=cpr) for cpr in set(row.cpr for row in rows)
+        }
+        Person.objects.bulk_create(
+            persons.values(),
+            update_conflicts=True,
+            update_fields=("cpr",),
+            unique_fields=("cpr",),
+        )
+        self.stdout.write(f"Processed {len(persons)} Person objects")
+
+        # Create or update Employer objects
+        employers = {cvr: Employer(cvr=cvr) for cvr in set(row.cvr for row in rows)}
+        Employer.objects.bulk_create(
+            employers.values(),
+            update_conflicts=True,
+            update_fields=("cvr",),
+            unique_fields=("cvr",),
+        )
+        self.stdout.write(f"Processed {len(employers)} Employer objects")
+
+        # Create or update PersonYear objects
+        person_years = [
+            PersonYear(person=person, year=self._year) for person in persons.values()
+        ]
+        PersonYear.objects.bulk_create(
+            person_years,
+            update_conflicts=True,
+            update_fields=("person", "year"),
+            unique_fields=("person", "year"),
+        )
+        self.stdout.write(f"Processed {len(person_years)} PersonYear objects")
+
+        # Create or update PersonMonth objects
+        person_months = {}
+        for person_year in person_years:
+            for month in range(1, 13):
+                person_month = PersonMonth(
+                    person_year=person_year,
+                    month=month,
+                    import_date=date.today(),
                 )
-                person_year, _ = PersonYear.objects.get_or_create(
-                    person=person, year=year
-                )
-                employer, _ = Employer.objects.get_or_create(cvr=line.cvr, defaults={})
-                for month, amount in enumerate(line.amounts, 1):
-                    person_month, _ = PersonMonth.objects.get_or_create(
-                        person_year=person_year,
-                        month=month,
-                        defaults={
-                            "import_date": date.today(),
-                        },
-                    )
-                    ASalaryReport.objects.update_or_create(
-                        person_month=person_month,
-                        employer=employer,
-                        defaults={
-                            "amount": amount,
-                        },
-                    )
-            print(i, end="\r")
+                key = (person_year.person.cpr, month)
+                person_months[key] = person_month
+        PersonMonth.objects.bulk_create(
+            person_months.values(),
+            update_conflicts=True,
+            update_fields=("import_date",),
+            unique_fields=("person_year", "month"),
+        )
+        self.stdout.write(f"Processed {len(person_months)} PersonMonth objects")
+
+        # Create ASalaryReport objects (existing objects for this year will be deleted!)
+        a_salary_reports = [
+            ASalaryReport(
+                person_month=person_months[(row.cpr, (index % 12) + 1)],
+                employer=employers[row.cvr],
+                amount=amount,
+            )
+            for row in rows
+            for index, amount in enumerate(row.amounts)
+        ]
+        ASalaryReport.objects.filter(
+            person_month__person_year__year=self._year
+        ).delete()
+        ASalaryReport.objects.bulk_create(a_salary_reports)
+        self.stdout.write(f"Created {len(a_salary_reports)} ASalaryReport objects")
