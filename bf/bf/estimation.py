82fbd79b4f29048b410df4aa4e4db743139c0d61
Mads Riis-Weitling <mrw@magenta.dk>
2024-07-04 16:56:36 +0200

    perf: [#61082] Optimize `estimate_income`
    This alters the interface between `estimate_income` and the
    `EstimationEngine` implementations.
    
    We now pass a list of rows (`subset`) to the `estimate` method, rather
    than two querysets of `MonthlyAIncomeReport` and `MonthlyBIncomeReport`.
    
    Fetching the data (`PersonMonth` and the associated sums of A and B
    income for each month) is now done in a single query in `_get_data_qs`,
    rather than fetching it individually in each call to `estimate`.
    
    To test:
    * `docker compose exec bf bash -c 'python manage.py estimate_income -v2
    --dry 2022'
    * `docker compose exec bf bash -c 'python manage.py estimate_income -v2
    --dry 2023'

--- bf/bf/estimation.py
+++ bf/bf/estimation.py
@@ -4,23 +4,18 @@
 from decimal import Decimal
 
 from data_analysis.models import IncomeEstimate
-from django.db.models import Q, QuerySet
-
-from bf.models import (
-    MonthlyAIncomeReport,
-    MonthlyBIncomeReport,
-    MonthlyIncomeReport,
-    PersonMonth,
-)
 
 
 class EstimationEngine:
     @classmethod
     def estimate(
         cls,
-        a_reports: QuerySet[MonthlyAIncomeReport],  # A income
-        b_reports: QuerySet[MonthlyBIncomeReport],  # B income
-        person_month: PersonMonth,
+        # a_reports: QuerySet[MonthlyAIncomeReport],  # A income
+        # b_reports: QuerySet[MonthlyBIncomeReport],  # B income
+        # person_month: PersonMonth,
+        subset: list,
+        year: int,
+        month: int,
     ) -> IncomeEstimate | None:
         raise NotImplementedError
 
@@ -40,29 +35,43 @@ class InYearExtrapolationEngine(EstimationEngine):
     @classmethod
     def estimate(
         cls,
-        a_reports: QuerySet[MonthlyAIncomeReport],
-        b_reports: QuerySet[MonthlyBIncomeReport],
-        person_month: PersonMonth,
+        # a_reports: QuerySet[MonthlyAIncomeReport],
+        # b_reports: QuerySet[MonthlyBIncomeReport],
+        # person_month: PersonMonth,
+        subset: list,
+        year: int,
+        month: int,
     ) -> IncomeEstimate | None:
-        year = person_month.year
-        month = person_month.month
-        amount_sum = cls.queryset_sum(a_reports, year, month) + cls.queryset_sum(
-            b_reports, year, month
-        )
+        # year = person_month.year
+        # month = person_month.month
+        # amount_sum = cls.queryset_sum(a_reports, year, month) + cls.queryset_sum(
+        #     b_reports, year, month
+        # )
+        amount_sum = cls.subset_sum(subset, year, month)
         year_estimate = int(12 * (amount_sum / month))
         return IncomeEstimate(
             estimated_year_result=year_estimate,
-            person_month=person_month,
+            # person_month=person_month,
             engine=cls.__name__,
         )
 
+    # @classmethod
+    # def queryset_sum(cls, qs: QuerySet, year: int, month: int) -> Decimal:
+    #     # Do not use any properties on the class as annotation names.
+    #     # Django will explode trying to put values into the properties.
+    #     qs = qs.filter(year=year, month__in=range(1, month + 1))
+    #     # print(qs.explain())
+    #     return MonthlyIncomeReport.sum_queryset(qs)
+
     @classmethod
-    def queryset_sum(cls, qs: QuerySet, year: int, month: int) -> Decimal:
-        # Do not use any properties on the class as annotation names.
-        # Django will explode trying to put values into the properties.
-        qs = qs.filter(year=year, month__in=range(1, month + 1))
-        # print(qs.explain())
-        return MonthlyIncomeReport.sum_queryset(qs)
+    def subset_sum(cls, subset: list, year: int, month: int) -> Decimal:
+        return sum(
+            [
+                row["_a_amount"] + row["_b_amount"]
+                for row in subset
+                if row["_year"] == year and row["_month"] in range(1, month + 1)
+            ]
+        )
 
 
 class TwelveMonthsSummationEngine(EstimationEngine):
@@ -71,28 +80,48 @@ class TwelveMonthsSummationEngine(EstimationEngine):
     @classmethod
     def estimate(
         cls,
-        a_reports: QuerySet[MonthlyAIncomeReport],
-        b_reports: QuerySet[MonthlyBIncomeReport],
-        person_month: PersonMonth,
+        # a_reports: QuerySet[MonthlyAIncomeReport],
+        # b_reports: QuerySet[MonthlyBIncomeReport],
+        # person_month: PersonMonth,
+        subset: list,
+        year: int,
+        month: int,
     ) -> IncomeEstimate | None:
 
-        year = person_month.year
-        month = person_month.month
-        year_estimate = cls.queryset_sum(a_reports, year, month) + cls.queryset_sum(
-            b_reports, year, month
-        )
+        # year = person_month.year
+        # month = person_month.month
+        # year_estimate = cls.queryset_sum(a_reports, year, month) + cls.queryset_sum(
+        #     b_reports, year, month
+        # )
+        year_estimate = cls.subset_sum(subset, year, month)
         return IncomeEstimate(
             estimated_year_result=year_estimate,
-            person_month=person_month,
+            # person_month=person_month,
             engine=cls.__name__,
         )
 
+    # @classmethod
+    # def queryset_sum(cls, qs: QuerySet, year: int, month: int) -> Decimal:
+    #     # Do not use any properties on the class as annotation names.
+    #     # Django will explode trying to put values into the properties.
+    #     qs = qs.filter(
+    #         Q(year=year, month__in=range(1, month + 1))
+    #         | Q(year=year - 1, month__in=range(month + 1, 13))
+    #     )
+    #     return MonthlyIncomeReport.sum_queryset(qs)
+
     @classmethod
-    def queryset_sum(cls, qs: QuerySet, year: int, month: int) -> Decimal:
-        # Do not use any properties on the class as annotation names.
-        # Django will explode trying to put values into the properties.
-        qs = qs.filter(
-            Q(year=year, month__in=range(1, month + 1))
-            | Q(year=year - 1, month__in=range(month + 1, 13))
+    def subset_sum(cls, subset: list, year: int, month: int) -> Decimal:
+        return sum(
+            [
+                row["_a_amount"] + row["_b_amount"]
+                for row in subset
+                if (
+                    (row["_year"] == year and row["_month"] in range(1, month + 1))
+                    or (
+                        row["_year"] == (year - 1)
+                        and row["_month"] in range(month + 1, 13)
+                    )
+                )
+            ]
         )
-        return MonthlyIncomeReport.sum_queryset(qs)
