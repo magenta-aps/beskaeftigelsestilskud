c68f0d97cdeaccd29510bf17be75739d21f233b8
Nick Janssen <nj@magenta-aps.dk>
2024-08-13 09:01:01 +0200

    chore: [#61666] Shell script to load data and estimate income

---
+++ load_data.sh
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2024 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: bash `basename $0` [amount of lines to load]"
+  echo "Note: Requires 2022_a.csv and 2023_a.csv files to be present in /bf. The files are attached to https://redmine.magenta.dk/issues/61082"
+  exit 0
+fi
+
+docker exec bf bash -c "python manage.py load_csv --count=$1 /app/2022_a.csv 2022"
+docker exec bf bash -c "python manage.py load_csv --count=$1 /app/2023_a.csv 2023"
+docker exec bf bash -c "python manage.py estimate_income 2022"
+docker exec bf bash -c "python manage.py estimate_income 2023"
