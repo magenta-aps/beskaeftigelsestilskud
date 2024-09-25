5f70fc6fe3a78aa26014a24fab5d6d1af899b516
Lars Peter Thomsen <larsp@magenta-aps.dk>
2024-09-25 08:05:06 +0200

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Carsten Agger <carstena@magenta-aps.dk>

--- bf/project/util.py
+++ bf/project/util.py
@@ -57,5 +57,5 @@ def mean_error(correct: Decimal, estimates: Iterable[Decimal]) -> Decimal:
 def root_mean_sq_error(correct: Decimal, estimates: Iterable[Decimal]) -> Decimal:
     # Root-mean-squared-error
     return Decimal(
-        math.sqrt(sum([(estimate - correct) ** 2 for estimate in estimates]) / 12)
+        math.sqrt(sum([(estimate - correct) ** 2 for estimate in estimates]) / len(estimates))
     )
