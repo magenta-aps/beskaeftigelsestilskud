1e603137e4a78599cf5f0fc1ad9c914ab643eafa
Mads Riis-Weitling <mrw@magenta.dk>
2024-10-22 15:08:58 +0200

    feat: [#62552] Add `Person{Search,Detail}View` and templates
    Also, introduce breadcrumb navigation using Bootstrap breadcrumb markup.
    This removes the "house" icon from the main nav bar, replacing it with a
    permanent `Home` entry in the breadcrumb.

--- bf/bf/urls.py
+++ bf/bf/urls.py
@@ -6,11 +6,13 @@ from typing import List
 
 from django.urls import URLPattern, URLResolver, path
 
-from bf.views import RootView
+from bf.views import PersonDetailView, PersonSearchView, RootView
 
 app_name = "bf"
 
 
 urlpatterns: List[URLResolver | URLPattern] = [
     path("", RootView.as_view(), name="root"),
+    path("persons", PersonSearchView.as_view(), name="person_search"),
+    path("persons/<int:pk>/", PersonDetailView.as_view(), name="person_detail"),
 ]
--- bf/bf/views.py
+++ bf/bf/views.py
@@ -1,10 +1,48 @@
 # SPDX-FileCopyrightText: 2024 Magenta ApS <info@magenta.dk>
 #
 # SPDX-License-Identifier: MPL-2.0
-
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, TemplateView
+from django_filters import FilterSet
+from django_filters.views import FilterView
+from django_tables2 import LinkColumn, SingleTableMixin, Table
+from django_tables2.utils import Accessor
 from login.view_mixins import LoginRequiredMixin
+
+from bf.models import Person
 
 
 class RootView(LoginRequiredMixin, TemplateView):
     template_name = "bf/root.html"
+
+
+class PersonTable(Table):
+    class Meta:
+        model = Person
+        fields = ("cpr", "name", "full_address", "civil_state", "location_code")
+
+    cpr = LinkColumn("bf:person_detail", args=[Accessor("pk")])
+
+
+class PersonFilterSet(FilterSet):
+    class Meta:
+        model = Person
+        fields = {
+            "cpr": ["exact"],
+            "name": ["icontains"],
+            "full_address": ["icontains"],
+            "civil_state": ["exact"],
+            "location_code": ["exact"],
+        }
+
+
+class PersonSearchView(LoginRequiredMixin, SingleTableMixin, FilterView):
+    model = Person
+    table_class = PersonTable
+    filterset_class = PersonFilterSet
+    template_name = "bf/person_search.html"
+
+
+class PersonDetailView(LoginRequiredMixin, DetailView):
+    model = Person
+    context_object_name = "person"
+    template_name = "bf/person_detail.html"
