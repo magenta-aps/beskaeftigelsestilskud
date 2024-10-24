e9622948caea79eb9eab07a5d9778f1f4f2927a1
Lars Peter Thomsen <larsp@magenta.dk>
2024-10-24 13:25:35 +0200

    fix: [#62540] NTLM auth

--- bf/project/settings/eskat.py
+++ bf/project/settings/eskat.py
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: MPL-2.0
 import os
 
+from project.util import strtobool
+
 ESKAT_BASE_URL = os.environ.get("ESKAT_BASE_URL")
 ESKAT_USERNAME = os.environ.get("ESKAT_USERNAME")
 ESKAT_PASSWORD = os.environ.get("ESKAT_PASSWORD")
+ESKAT_VERIFY = strtobool(os.environ.get("ESKAT_VERIFY", True), True)
