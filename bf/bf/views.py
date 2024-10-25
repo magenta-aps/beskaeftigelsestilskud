ef6aeee400eefd3fd8b6861f2be43a86905acc97
Mads Riis-Weitling <mrw@magenta.dk>
2024-10-25 16:19:25 +0200

    feat: [#62552] Improve search filters on "category" fields; provide i18n

--- bf/bf/views.py
+++ bf/bf/views.py
@@ -1,8 +1,12 @@
 # SPDX-FileCopyrightText: 2024 Magenta ApS <info@magenta.dk>
 #
 # SPDX-License-Identifier: MPL-2.0
+from typing import Callable
+
+from django.db.models import Count, Field
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, TemplateView
-from django_filters import FilterSet
+from django_filters import CharFilter, ChoiceFilter, FilterSet
 from django_filters.views import FilterView
 from django_tables2 import LinkColumn, SingleTableMixin, Table
 from django_tables2.utils import Accessor
@@ -23,16 +27,43 @@ class PersonTable(Table):
     cpr = LinkColumn("bf:person_detail", args=[Accessor("pk")])
 
 
+class CategoryChoiceFilter(ChoiceFilter):
+    _isnull = "isnull"
+
+    def __init__(self, *args, **kwargs):
+        field = kwargs.pop("field")
+        super().__init__(*args, choices=self._get_choices(field.field), **kwargs)
+
+    def filter(self, qs, value):
+        if value == self._isnull:
+            return self.get_method(qs)(**{"%s__isnull" % self.field_name: True})
+        return super().filter(qs, value)  # pragma: no cover
+
+    def _get_choices(self, field: Field) -> Callable:
+        def _func():
+            return [
+                (
+                    val[field.name] or self._isnull,
+                    f"{val[field.name] or _('Ingen')} ({val['count']})",
+                )
+                for val in field.model.objects.values(field.name)
+                .annotate(count=Count("id"))
+                .order_by(field.name)
+            ]
+
+        return _func
+
+
 class PersonFilterSet(FilterSet):
-    class Meta:
-        model = Person
-        fields = {
-            "cpr": ["exact"],
-            "name": ["icontains"],
-            "full_address": ["icontains"],
-            "civil_state": ["exact"],
-            "location_code": ["exact"],
-        }
+    cpr = CharFilter("cpr", label=_("CPR-nummer"))
+    name = CharFilter("name", lookup_expr="icontains", label=_("Navn"))
+    full_address = CharFilter(
+        "full_address", lookup_expr="icontains", label=_("Adresse")
+    )
+    civil_state = CategoryChoiceFilter(field=Person.civil_state, label=_("Civilstand"))
+    location_code = CategoryChoiceFilter(
+        field=Person.location_code, label=_("Stedkode")
+    )
 
 
 class PersonSearchView(LoginRequiredMixin, SingleTableMixin, FilterView):
