84b186eca3a22c4f1dcd11dd99c818d305626243
Mads Riis-Weitling <mrw@magenta.dk>
2024-10-31 15:35:14 +0100

    feat: [#62552] Show estimated and actual year result plus benefit paid to date

--- bf/bf/views.py
+++ bf/bf/views.py
@@ -1,10 +1,12 @@
 # SPDX-FileCopyrightText: 2024 Magenta ApS <info@magenta.dk>
 #
 # SPDX-License-Identifier: MPL-2.0
+from datetime import date
 from typing import Callable
 
 from django.db.models import CharField, Count, Field, Value
 from django.db.models.functions import Cast, LPad
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, TemplateView
 from django_filters import CharFilter, ChoiceFilter, FilterSet
@@ -15,6 +17,7 @@ from django_tables2.utils import Accessor
 from login.view_mixins import LoginRequiredMixin
 
 from bf.models import Person
+from bf.querysets import PersonKeyFigureQuerySet
 
 
 class RootView(LoginRequiredMixin, TemplateView):
@@ -35,6 +38,21 @@ class PersonTable(Table):
     full_address = Column(verbose_name=_("Adresse"))
     civil_state = Column(verbose_name=_("Civilstand"))
     location_code = Column(verbose_name=_("Stedkode"))
+    total_estimated_year_result = Column(
+        accessor=Accessor("_total_estimated_year_result"),
+        order_by=Accessor("_total_estimated_year_result"),
+        verbose_name=_("Forventet samlet indtægt i indeværende år"),
+    )
+    total_actual_year_result = Column(
+        accessor=Accessor("_total_actual_year_result"),
+        order_by=Accessor("_total_actual_year_result"),
+        verbose_name=_("Faktisk samlet indtægt i indeværende år"),
+    )
+    benefit_paid = Column(
+        accessor=Accessor("_benefit_paid"),
+        order_by=Accessor("_benefit_paid"),
+        verbose_name=_("Beskæftigelsesfradrag til dato i indeværende år"),
+    )
 
 
 class CategoryChoiceFilter(ChoiceFilter):
@@ -77,14 +95,25 @@ class PersonFilterSet(FilterSet):
 
 
 class PersonSearchView(LoginRequiredMixin, SingleTableMixin, FilterView):
+    model = Person
     table_class = PersonTable
     filterset_class = PersonFilterSet
     template_name = "bf/person_search.html"
 
     def get_queryset(self):
-        return Person.objects.annotate(
-            _cpr=LPad(Cast("cpr", CharField()), 10, Value("0"))
-        ).order_by("_cpr")
+        # Get "key figure" queryset for current year and month
+        today: date = timezone.now().date()
+        today = date(2020, 12, 1)  # no commit
+        qs = PersonKeyFigureQuerySet.from_queryset(
+            super().get_queryset(),
+            year=today.year,
+            month=today.month,
+        )
+        # Add zero-padded text version of CPR to ensure proper display and sorting
+        qs = qs.annotate(_cpr=LPad(Cast("cpr", CharField()), 10, Value("0")))
+        # Set initial sorting (can be overridden by user)
+        qs = qs.order_by("_cpr")
+        return qs
 
 
 class PersonDetailView(LoginRequiredMixin, DetailView):
