4e89308837de6699b0c82d9655ecdd2ecc301c4c
Mads Riis-Weitling <mrw@magenta.dk>
2024-11-08 12:27:25 +0100

    feat: [#61669] Add `PrismeAccountAliasAdmin`
    A small modification to the `bf_tags.yesno` template tag is necessary in
    order to keep the Django admin templates working (they also use the
    `yesno` filter.)

---
+++ bf/bf/admin.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2024 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+from django.contrib import admin
+
+from bf.models import PrismeAccountAlias
+
+
+@admin.register(PrismeAccountAlias)
+class PrismeAccountAliasAdmin(admin.ModelAdmin):
+    list_display = [
+        "tax_municipality_location_code",
+        "tax_year",
+        "alias",
+        "tax_municipality_six_digit_code",
+    ]
+    ordering = [
+        "tax_municipality_location_code",
+        "tax_year",
+        "alias",
+    ]
+    list_filter = [
+        "tax_municipality_location_code",
+        "tax_year",
+    ]
+    search_fields = ["alias__icontains"]
