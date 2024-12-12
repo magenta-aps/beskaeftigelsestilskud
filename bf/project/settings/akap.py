0419e6b9b7f8cc5c56793335090160a8f701f13f
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-12-12 15:25:02 +0100

    feat: [#63151] new settings for AKAP API

---
+++ bf/project/settings/akap.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2024 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+import os
+
+AKAP_HOST = os.environ.get("AKAP_HOST", "https://akap.sullissivik.gl")
+AKAP_API_SECRET = os.environ.get("AKAP_API_SECRET", "supersecret")
