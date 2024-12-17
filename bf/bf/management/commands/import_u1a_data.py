c17b24c4a915641ce3b2a065ea1675924b556820
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-12-17 11:59:45 +0100

    feat: [#63151] management 'import_u1a_data' can now import u1a entries based on year/regnskabsår

--- bf/bf/management/commands/import_u1a_data.py
+++ bf/bf/management/commands/import_u1a_data.py
@@ -5,13 +5,13 @@
 import datetime
 import logging
 import time
-from typing import Optional
+from typing import Optional, Tuple
 
 from django.conf import settings
 from django.db import transaction
 from pydantic import BaseModel
 
-from bf.akap import get_akap_u1a_entries, get_akap_u1a_items
+from bf.akap import AKAPU1A, AKAPU1AItem, get_akap_u1a_entries, get_akap_u1a_items
 from bf.management.commands.common import BfBaseCommand
 from bf.models import U1AEntry, U1AItemEntry
 
@@ -48,10 +48,10 @@ class Command(BfBaseCommand):
         try:
             if not year and not cpr:
                 logger.info("Importing: All entries")
-                result = self._import_everything(dry)
+                result = self._import_everything()
             elif year and not cpr:
                 logger.info(f"Importing: Entries from {year}")
-                result = self._import_year(dry)
+                result = self._import_year(int(year))
                 pass
             elif not year and cpr:
                 logger.info(f"Importing: Entries by {cpr}")
@@ -84,7 +84,7 @@ class Command(BfBaseCommand):
         logger.info(f"U1AItemEntries updated: {result.updated_items}")
         logger.info(f"Exec time: {duration.strftime('%H:%M:%S')}")
 
-    def _import_everything(self, dry: bool = False) -> ImportResult:
+    def _import_everything(self) -> ImportResult:
         result = ImportResult()
 
         akap_u1as = get_akap_u1a_entries(
@@ -94,6 +94,12 @@ class Command(BfBaseCommand):
         )
 
         for u1a in akap_u1as:
+            db_u1a_entry, u1a_created = self._create_or_update_u1a(u1a)
+            if u1a_created:
+                result.new_entries += 1
+            else:
+                result.updated_entries += 1
+
             u1a.items = get_akap_u1a_items(
                 settings.AKAP_HOST,  # type: ignore[misc]
                 settings.AKAP_API_SECRET,  # type: ignore[misc]
@@ -101,32 +107,73 @@ class Command(BfBaseCommand):
                 fetch_all=True,
             )
 
-            qs_current_u1a_entry = U1AEntry.objects.filter(u1a_id=u1a.id)
-            db_u1a_entry: Optional[U1AEntry] = None
-            if qs_current_u1a_entry.exists():
-                if not dry:
-                    qs_current_u1a_entry.update(**u1a.model_dict)
-
-                db_u1a_entry = U1AEntry.objects.get(u1a_id=u1a.id)
-                result.updated_entries += 1
-            else:
-                db_u1a_entry = U1AEntry.objects.create(**u1a.model_dict)
-                result.new_entries += 1
-
             for u1a_item in u1a.items:
-                qs_u1a_item_current = U1AItemEntry.objects.filter(
-                    u1a_item_id=u1a_item.id
+                _, u1a_item_created = self._create_or_update_u1a_item(
+                    db_u1a_entry.id, u1a_item
                 )
-
-                if qs_u1a_item_current.exists():
-                    qs_u1a_item_current.update(
-                        **{**u1a_item.model_dict, "u1a_entry_id": db_u1a_entry.id}
-                    )
-                    result.updated_items += 1
-                else:
-                    U1AItemEntry.objects.create(
-                        **{**u1a_item.model_dict, "u1a_entry_id": db_u1a_entry.id}
-                    )
+                if u1a_item_created:
                     result.new_items += 1
+                else:
+                    result.updated_items += 1
 
         return result
+
+    def _import_year(self, year: int) -> ImportResult:
+        result = ImportResult()
+
+        akap_u1as = get_akap_u1a_entries(
+            settings.AKAP_HOST,  # type: ignore[misc]
+            settings.AKAP_API_SECRET,  # type: ignore[misc]
+            year=year,
+            fetch_all=True,
+        )
+
+        for u1a in akap_u1as:
+            db_u1a_entry, u1a_created = self._create_or_update_u1a(u1a)
+            if u1a_created:
+                result.new_entries += 1
+            else:
+                result.updated_entries += 1
+
+            u1a.items = get_akap_u1a_items(
+                settings.AKAP_HOST,  # type: ignore[misc]
+                settings.AKAP_API_SECRET,  # type: ignore[misc]
+                u1a.id,
+                fetch_all=True,
+            )
+
+            for u1a_item in u1a.items:
+                _, u1a_item_created = self._create_or_update_u1a_item(
+                    db_u1a_entry.id, u1a_item
+                )
+                if u1a_item_created:
+                    result.new_items += 1
+                else:
+                    result.updated_items += 1
+
+        return result
+
+    def _create_or_update_u1a(self, akap_u1a: AKAPU1A) -> Tuple[U1AEntry, bool]:
+        qs_current_u1a_entry = U1AEntry.objects.filter(u1a_id=akap_u1a.id)
+        if qs_current_u1a_entry.exists():
+            qs_current_u1a_entry.update(**akap_u1a.model_dict)
+            return U1AEntry.objects.get(u1a_id=akap_u1a.id), 0
+
+        return U1AEntry.objects.create(**akap_u1a.model_dict), 1
+
+    def _create_or_update_u1a_item(
+        self, db_u1a_entry_id: int, akap_u1a_item: AKAPU1AItem
+    ) -> Tuple[U1AItemEntry, bool]:
+        qs_u1a_item_current = U1AItemEntry.objects.filter(u1a_item_id=akap_u1a_item.id)
+        if qs_u1a_item_current.exists():
+            qs_u1a_item_current.update(
+                **{**akap_u1a_item.model_dict, "u1a_entry_id": db_u1a_entry_id}
+            )
+            return U1AItemEntry.objects.get(u1a_item_id=akap_u1a_item.id), 0
+
+        return (
+            U1AItemEntry.objects.create(
+                **{**akap_u1a_item.model_dict, "u1a_entry_id": db_u1a_entry_id}
+            ),
+            1,
+        )
