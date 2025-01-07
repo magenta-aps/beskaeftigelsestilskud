244ce4a260b565205968ed09bedac91b0ffc4587
Mads Riis-Weitling <mrw@magenta.dk>
2025-01-07 09:52:21 +0100

    fix: [#63436] Remove leading zeroes from Prisme alias "formaal" and "stedkode"

--- bf/bf/management/commands/load_prisme_account_aliases.py
+++ bf/bf/management/commands/load_prisme_account_aliases.py
@@ -14,38 +14,44 @@ class Command(BaseCommand):
 
         AFDELING = "100045"
         FINANSLOV = "240614"
-        FORMAAL = "0101010000"
+        FORMAAL = "101010000"
         ART = "242040195"
 
         MUNICIPALITY_CODES = [
             (
-                "010300",
+                # Kommune Kujalleq           31 (eSkat) og 10300 (Prisme SEL)
+                "10300",
                 "955",
-            ),  # Kommune Kujalleq           31 (eSkat) og 010300 (Prisme SEL)
+            ),
             (
-                "010400",
+                # Kommuneqarfik Sermersooq   32 (eSkat) og 10400 (Prisme SEL)
+                "10400",
                 "956",
-            ),  # Kommuneqarfik Sermersooq   32 (eSkat) og 010400 (Prisme SEL)
+            ),
             (
-                "010500",
+                # Qeqqata Kommunia           33 (eSkat) og 10500 (Prisme SEL)
+                "10500",
                 "957",
-            ),  # Qeqqata Kommunia           33 (eSkat) og 010500 (Prisme SEL)
+            ),
             (
-                "010600",
+                # Kommune Qeqertalik         36 (eSkat) og 10600 (Prisme SEL)
+                "10600",
                 "959",
-            ),  # Kommune Qeqertalik         36 (eSkat) og 010600 (Prisme SEL)
+            ),
             (
-                "010700",
+                # Avannaata Kommunia         37 (eSkat) og 10700 (Prisme SEL)
+                "10700",
                 "960",
-            ),  # Avannaata Kommunia         37 (eSkat) og 010700 (Prisme SEL)
+            ),
             (
-                "010900",
+                # SDI (Skattestyrelsen)      20 (eSkat) og 10900 (Prisme SEL)
+                "19000",
                 "961",
-            ),  # SDI (Skattestyrelsen)      20 (eSkat) og 010900 (Prisme SEL)
+            ),
         ]
 
-        # Currently, account aliases are defined for the years
-        # 2025, 2026, 2027, 2029 and 2030
+        # Currently, account aliases are defined for the years 2025, 2026, 2027, 2028,
+        # 2029 and 2030.
         TAX_YEARS = range(2025, 2031)
 
         for municipality_code, tax_year in itertools.product(
