452b7b0cb8d9f27e848fe9fb20f0cb68c95fd710
Lars Peter Thomsen <larsp@magenta.dk>
2025-01-16 13:45:48 +0100

    fix: [#63584] Fix fejlbeskeder under opstart

--- suila/suila/management/commands/eboks_update_status.py
+++ suila/suila/management/commands/eboks_update_status.py
@@ -6,6 +6,7 @@ from suila.models import EboksMessage
 
 
 class Command(SuilaBaseCommand):
+    filename = __file__
 
     def _handle(self, *args, **kwargs):
         EboksMessage.update_final_statuses()
