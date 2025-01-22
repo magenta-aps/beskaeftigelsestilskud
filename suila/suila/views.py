a4e0d97fff371866fb5842f84271bc12673d8fd2
Lars Peter Thomsen <larsp@magenta.dk>
2025-01-22 10:54:12 +0100

    fix: [#62978] Refactor

--- suila/suila/views.py
+++ suila/suila/views.py
@@ -4,7 +4,7 @@
 import itertools
 import json
 from functools import cached_property
-from typing import Callable
+from typing import Any, Callable
 from urllib.parse import urlencode
 
 from common.models import User
@@ -12,11 +12,11 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import CharField, Count, F, Field, Q, QuerySet, Sum, Value
 from django.db.models.functions import Cast, LPad
 from django.forms.models import BaseInlineFormSet
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import DetailView, TemplateView
+from django.views.generic import DetailView, FormView, TemplateView
 from django.views.generic.detail import BaseDetailView
 from django_filters import CharFilter, ChoiceFilter, FilterSet
 from django_filters.views import FilterView
@@ -38,7 +38,6 @@ from suila.models import (
 )
 from suila.querysets import PersonKeyFigureQuerySet
 from suila.templatetags.date_tags import month_name
-from suila.view_mixins import FormWithFormsetView
 
 
 class RootView(LoginRequiredMixin, TemplateView):
@@ -445,6 +444,62 @@ class PersonDetailIncomeView(
         # TODO: yield a row for each employer when we have it
         yield _("A-indkomst"), zero_pad(qs, "_a_income")
         yield _("B-indkomst"), zero_pad(qs, "_b_income")
+
+
+class FormWithFormsetView(FormView):
+    formset_class: Any = None
+
+    def get_formset(self, formset_class=None):
+        if formset_class is None:
+            formset_class = self.get_formset_class()
+        return formset_class(**self.get_formset_kwargs())
+
+    def get_formset_kwargs(self):
+        """Return the keyword arguments for instantiating the form."""
+        kwargs = {
+            "initial": self.get_initial(),
+            "prefix": self.get_prefix(),
+        }
+        if self.request.method in ("POST", "PUT"):
+            kwargs.update(
+                {
+                    "data": self.request.POST,
+                    "files": self.request.FILES,
+                }
+            )
+
+        return kwargs
+
+    def get_formset_class(self):
+        return self.formset_class
+
+    def get_context_data(self, **kwargs):
+        if "formset" not in kwargs:
+            kwargs["formset"] = self.get_formset()
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form, formset):
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form, formset):
+        return self.render_to_response(
+            self.get_context_data(form=form, formset=formset)
+        )
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        formset = self.get_formset()
+        for subform in formset:
+            if hasattr(subform, "set_parent_form"):
+                subform.set_parent_form(form)  # pragma: no cover
+        form.full_clean()
+        formset.full_clean()
+        if hasattr(form, "clean_with_formset"):
+            form.clean_with_formset(formset)  # pragma: no cover
+        if form.is_valid() and formset.is_valid():
+            return self.form_valid(form, formset)
+        else:
+            return self.form_invalid(form, formset)
 
 
 class PersonDetailNotesView(
