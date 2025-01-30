72007ab6f642773a84ef6cf50874ea1f7617dd52
Lars Peter Thomsen <larsp@magenta.dk>
2025-01-30 11:15:11 +0100

    feat: [#63295] Mitid testing utils

--- suila/mitid_test/apps.py
+++ suila/mitid_test/apps.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
 from django.apps import AppConfig
 
 
--- suila/mitid_test/views.py
+++ suila/mitid_test/views.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
 import json
 
 from django.contrib.sessions.models import Session
