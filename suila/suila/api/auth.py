9f7eebe6d324249128802da1c6ac007eb93c9473
Lars Peter Thomsen <larsp@magenta.dk>
2025-02-04 14:38:15 +0100

    fix: [#63295] Fix login

--- suila/suila/api/auth.py
+++ suila/suila/api/auth.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MPL-2.0
 # mypy: disable-error-code="call-arg, attr-defined"
 import re
-from typing import List, Tuple
+from typing import Dict
 from urllib.parse import unquote
 
 from common.models import User
@@ -43,26 +43,24 @@ class ClientCertAuth(AuthBase):
         return None
 
     @classmethod
-    def get_info(cls, request: HttpRequest) -> List[Tuple[str, str]] | None:
+    def get_info(cls, request: HttpRequest) -> Dict[str, str] | None:
         info = request.headers.get(cls.cert_info_header)
         if info is not None:
             info = unquote(info)
-            items = []
+            items = {}
             for part in re.findall(r'\w+="[^"]*"', info):
                 eq_index = part.index("=")
                 key: str = part[0:eq_index]
                 value: str = part[eq_index + 1 :].strip('"')
-                items.append((key, value))
+                items[key] = value
             return items
         return None
 
     @staticmethod
     def get_subject(request: HttpRequest) -> str | None:
         info = ClientCertAuth.get_info(request)
-        if info is not None and len(info) > 0:
-            for part in info:
-                if part[0] == "Subject":
-                    return part[1]
+        if info is not None:
+            return info.get("Subject")
         return None
 
     def authenticate(self, subject: str) -> User | None:
