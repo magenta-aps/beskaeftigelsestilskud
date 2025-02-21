7c14b13c820b7d9b4867735cb341308cb82c3061
Thor Astrup Pedersen <tap@magenta-aps.dk>
2025-02-21 17:56:30 +0100

    refactor: [#63709] implemented ThreadPoolExecutor in get_person_info_from_dafo, max_workers=5

--- suila/suila/management/commands/get_person_info_from_dafo.py
+++ suila/suila/management/commands/get_person_info_from_dafo.py
@@ -1,6 +1,9 @@
 # SPDX-FileCopyrightText: 2024 Magenta ApS <info@magenta.dk>
 #
 # SPDX-License-Identifier: MPL-2.0
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import Any, Tuple
+
 from common.pitu import PituClient
 from requests.exceptions import HTTPError
 
@@ -13,12 +16,13 @@ class Command(SuilaBaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("--cpr", type=str)
+        parser.add_argument("--maxworkers", type=int, default=5)
         super().add_arguments(parser)
 
     def _handle(self, *args, **kwargs):
         """Updates all Person objects based on CPR data from DAFO/Pitu"""
         self._verbose = kwargs["verbosity"] > 1
-        self._write_verbose("Loading CPR data from DAFO")
+        self._write_verbose("Loading CPR data from database ...")
         if kwargs["cpr"]:
             persons = Person.objects.filter(cpr=kwargs["cpr"])
             if not persons:
@@ -30,9 +34,9 @@ class Command(SuilaBaseCommand):
 
         pitu_client = self._get_pitu_client()
 
-        for person in persons:
+        def fetch_person(person: Person) -> Tuple[Person, Any]:
             try:
-                person_data = pitu_client.get_person_info(person.cpr)
+                return person, pitu_client.get_person_info(person.cpr)
             except HTTPError as e:
                 if e.response.status_code == 404:
                     self._write_verbose(
@@ -40,42 +44,79 @@ class Command(SuilaBaseCommand):
                     )
                 else:
                     self._write_verbose(
-                        f"Unexpected {e.response.status_code} error: "
-                        f"{e.response.content}"
+                        (
+                            f"Unexpected {e.response.status_code} "
+                            f"error: {e.response.content}"
+                        )
                     )
-            else:
-                if "civilstand" in person_data:
-                    person.civil_state = person_data["civilstand"]
-                else:
+                return None  # Indicate failure
+
+        maxworkers: int = kwargs["maxworkers"]
+        self._write_verbose(f"Starting person update-workers (max_worker={maxworkers})")
+        with ThreadPoolExecutor(max_workers=maxworkers) as executor:
+            future_to_person = {
+                executor.submit(fetch_person, person): person for person in persons
+            }
+
+            for future in as_completed(future_to_person):
+                try:
+                    future_tuple = future.result()
+                    if not future_tuple:
+                        continue
+
+                    person_model, fetched_person_data = future_tuple
+
+                    if "civilstand" in fetched_person_data:
+                        person_model.civil_state = fetched_person_data["civilstand"]
+                    else:
+                        self._write_verbose(
+                            f'no "civilstand" in person_data: {fetched_person_data}'
+                        )
+
+                    if "myndighedskode" in fetched_person_data:
+                        person_model.location_code = fetched_person_data[
+                            "myndighedskode"
+                        ]
+                    else:
+                        self._write_verbose(
+                            f'no "myndighedskode" in person_data: {fetched_person_data}'
+                        )
+
+                    if (
+                        "fornavn" in fetched_person_data
+                        and "efternavn" in fetched_person_data
+                    ):
+                        person_model.name = (
+                            f"{fetched_person_data['fornavn']} "
+                            f"{fetched_person_data['efternavn']}"
+                        )
+                    else:
+                        self._write_verbose(
+                            (
+                                'no "fornavn" and "efternavn" in '
+                                f"person_data: {fetched_person_data}"
+                            )
+                        )
+
+                    address = fetched_person_data.get("adresse")
+                    city = fetched_person_data.get("bynavn")
+                    post_code = fetched_person_data.get("postnummer")
+
+                    if all(val is not None for val in (address, city, post_code)):
+                        person_model.full_address = f"{address}, {post_code} {city}"
+
+                    person_model.save()
+
                     self._write_verbose(
-                        f'no "civilstand" in person_data: {person_data}'
+                        (
+                            f"Updated CPR data for {person_model.cpr} "
+                            f"(person data = {fetched_person_data})"
+                        )
                     )
-
-                if "myndighedskode" in person_data:
-                    person.location_code = person_data["myndighedskode"]
-                else:
+                except Exception as e:
                     self._write_verbose(
-                        f'no "myndighedskode" in person_data: {person_data}'
+                        f"Error processing person {person_model.cpr}: {e}"
                     )
-
-                if "fornavn" in person_data and "efternavn" in person_data:
-                    person.name = f"{person_data['fornavn']} {person_data['efternavn']}"
-                else:
-                    self._write_verbose(
-                        f'no "fornavn" and "efternavn" in person_data: {person_data}'
-                    )
-
-                address = person_data.get("adresse")
-                city = person_data.get("bynavn")
-                post_code = person_data.get("postnummer")
-                if all(val is not None for val in (address, city, post_code)):
-                    person.full_address = f"{address}, {post_code} {city}"
-
-                person.save()
-
-                self._write_verbose(
-                    f"Updated CPR data for {person.cpr} (person data = {person_data})"
-                )
 
         self._write_verbose("Done")
         pitu_client.close()
