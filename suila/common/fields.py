921dfd9ae573ecae96bb1c6abbcb6a1f432b94ac
Lars Peter Thomsen <larsp@magenta.dk>
2025-03-04 14:36:32 +0100

    fix: [#64128] CPR-bindestreg

--- suila/common/fields.py
+++ suila/common/fields.py
@@ -11,9 +11,10 @@ class CPRField(RegexField):
         super().__init__(r"^\d{6}-{0,1}\d{4}$", **kwargs)
 
     def to_python(self, raw_value: Any) -> str:
-        value: str = super().to_python(raw_value)
-        # Remove dash in CPR, if given
-        value = value.strip()
-        if "-" in value:
-            value = value.replace("-", "")
+        value: str | None = super().to_python(raw_value)
+        if value is not None:
+            # Remove dash in CPR, if given
+            value = value.strip()
+            if "-" in value:
+                value = value.replace("-", "")
         return value
