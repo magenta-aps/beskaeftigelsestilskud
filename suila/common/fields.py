6aeda781a8d7f0c1a7cbb540ee76f15ba397d85b
Lars Peter Thomsen <larsp@magenta.dk>
2025-03-04 14:30:24 +0100

    fix: [#64128] CPR-bindestreg

--- suila/common/fields.py
+++ suila/common/fields.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
 #
 # SPDX-License-Identifier: MPL-2.0
+from typing import Any
+
 from django.forms import RegexField
 
 
@@ -8,11 +10,10 @@ class CPRField(RegexField):
     def __init__(self, **kwargs):
         super().__init__(r"^\d{6}-{0,1}\d{4}$", **kwargs)
 
-    def to_python(self, value):
-        value = super().to_python(value)
+    def to_python(self, raw_value: Any) -> str:
+        value: str = super().to_python(raw_value)
         # Remove dash in CPR, if given
-        if isinstance(value, str):
-            value = value.strip()
-            if "-" in value:
-                value = value.replace("-", "")
+        value = value.strip()
+        if "-" in value:
+            value = value.replace("-", "")
         return value
