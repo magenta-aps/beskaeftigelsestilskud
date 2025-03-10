3c92b195a397b2df41a57c53fc34b1dafdeb0b59
Thor Astrup Pedersen <tap@magenta-aps.dk>
2025-03-10 11:14:04 +0100

    refactor: [#64096] MonthlyIncomeReport.update_amount() DOES NOT set u_income from year anymore

--- suila/suila/models.py
+++ suila/suila/models.py
@@ -912,7 +912,7 @@ class MonthlyIncomeReport(PermissionsMixin, models.Model):
             + self.employer_paid_gl_pension_income
             + self.catchsale_income
         ).quantize(q)
-        self.u_income = Decimal(self.person_month.u_income_from_year).quantize(q)
+        self.u_income = self.u_income.quantize(q)
 
     @staticmethod
     def annotate_month(
