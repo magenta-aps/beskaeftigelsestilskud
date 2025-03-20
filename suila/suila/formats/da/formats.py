10bc1063c0f3c13d819260fcca64a5e96f095c23
Mads Riis-Weitling <mrw@magenta.dk>
2025-03-20 13:20:22 +0100

    feat: [#63127] Define our `DATE_FORMAT`, etc. for `da` and `kl` locales

---
+++ suila/suila/formats/da/formats.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+DATE_FORMAT = "j. E Y"
+DATETIME_FORMAT = "d.m.Y H:i"
+TIME_FORMAT = "H:i"
+SHORT_DATE_FORMAT = "d.m.Y"
