2e8788cf8291a9293c28237e4dc8ae60802a331c
Mads Riis-Weitling <mrw@magenta.dk>
2025-03-20 15:11:38 +0100

    fix: [#63127] Use correct date format for `gl` locale ("Marsip 18, 2025")
    This uses the month names in `django.utils.dates.MONTH_ALT` for the
    genitive forms of Greenlandic month names.

--- suila/suila/formats/kl/formats.py
+++ suila/suila/formats/kl/formats.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
 #
 # SPDX-License-Identifier: MPL-2.0
-DATE_FORMAT = "j. E Y"
+DATE_FORMAT = "E j, Y"
 DATETIME_FORMAT = "d.m.Y H:i"
 TIME_FORMAT = "H:i"
 SHORT_DATE_FORMAT = "d.m.Y"
