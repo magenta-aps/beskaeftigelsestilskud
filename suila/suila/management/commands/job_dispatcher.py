e89b7062ad920f1e9cc3f8d1e7f181001f6ec48e
Thor Astrup Pedersen <tap@magenta-aps.dk>
2025-04-04 13:20:39 +0200

    feat: [#64642] job_dispatcher management command now inherits from SuilaBaseCommand

--- suila/suila/management/commands/job_dispatcher.py
+++ suila/suila/management/commands/job_dispatcher.py
@@ -6,13 +6,14 @@
 # Intended to be run daily
 
 from django.conf import settings
-from django.core.management.base import BaseCommand
 
 from suila.dispatch import JobDispatcher
+from suila.management.commands.common import SuilaBaseCommand
 from suila.models import ManagementCommands
 
 
-class Command(BaseCommand):
+class Command(SuilaBaseCommand):
+    filename = __file__
 
     def add_arguments(self, parser):
         parser.add_argument("--year", type=int)
@@ -22,7 +23,11 @@ class Command(BaseCommand):
         parser.add_argument("--reraise", action="store_true", default=False)
         super().add_arguments(parser)
 
-    def handle(self, *args, **options):
+    def _write_verbose(self, msg, **kwargs):
+        if self._verbose:
+            self.stdout.write(msg, **kwargs)
+
+    def _handle(self, *args, **options):
         """
         Job which runs all other relevant management jobs on the proper days.
 
@@ -147,7 +152,3 @@ class Command(BaseCommand):
         )
 
         self._write_verbose("Done")
-
-    def _write_verbose(self, msg, **kwargs):
-        if self._verbose:
-            self.stdout.write(msg, **kwargs)
