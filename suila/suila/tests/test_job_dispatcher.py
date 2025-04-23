0442bc454ff1d73716daa6fc7bf8c8f33a75fac3
Thor Astrup Pedersen <tap@magenta-aps.dk>
2025-04-23 14:56:41 +0200

    test: [#64642] new TestJobDispatcherCommands to verify job_dispatcher calls on specific dates

---
+++ suila/suila/tests/test_job_dispatcher.py
@@ -0,0 +1,138 @@
+# SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from datetime import datetime, timedelta
+from io import StringIO
+from unittest.mock import MagicMock, call, patch
+
+from django.core.management import call_command
+from django.test import TestCase, override_settings
+from django.utils import timezone
+
+from suila.management.commands.job_dispatcher import Command as JobDispatcherCommand
+from suila.models import JobLog, ManagementCommands, StatusChoices
+
+
+class TestJobDispatcherCommands(TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        super().setUp()
+        self.command = JobDispatcherCommand()
+        self.command.stdout = StringIO()
+
+    @patch("suila.dispatch.management.call_command")
+    @override_settings(ESKAT_BASE_URL="http://djangotest")
+    def test_job_dispatch_calls_second_tuesday(
+        self,
+        mock_call_command: MagicMock,
+    ):
+        # JobDispatcher date which is the second "tuesday" in the month
+        job_dispatcher_test_date = datetime(2025, 4, 8)
+
+        # Mocking
+        def _mock_call_command_logic(command_name, *args, **options):
+            # Handle command "_param"'s
+            cmd_param_year = (
+                options.get("year", None)
+                if command_name not in ["load_eskat"]
+                else args[0]
+            )
+
+            cmd_param_type = (
+                options.get("type", None)
+                if command_name not in ["load_eskat"]
+                else args[1]
+            )
+            cmd_param_month = options.get("month", None)
+
+            # Create JobLog entry for command
+            now = timezone.now()
+            JobLog.objects.create(
+                name=command_name,
+                status=StatusChoices.SUCCEEDED,
+                year_param=cmd_param_year,
+                month_param=cmd_param_month,
+                type_param=cmd_param_type,
+                # Fake it took 30 seconds to run the job
+                runtime=now,
+                runtime_end=now + timedelta(seconds=30),
+            )
+
+        mock_call_command.side_effect = _mock_call_command_logic
+
+        # Invoke the 'job_dispatcher' with the specified date
+        call_command(
+            self.command,
+            year=job_dispatcher_test_date.year,
+            month=job_dispatcher_test_date.month,
+            day=job_dispatcher_test_date.day,
+        )
+
+        # Assert
+        mock_call_command.assert_has_calls(
+            [
+                # "data load"-jobs
+                call(
+                    ManagementCommands.LOAD_ESKAT,
+                    job_dispatcher_test_date.year,
+                    "expectedincome",
+                    month=None,
+                    cpr=None,
+                    skew=False,
+                    traceback=False,
+                    reraise=False,
+                    verbosity=1,
+                ),
+                call(
+                    ManagementCommands.LOAD_ESKAT,
+                    job_dispatcher_test_date.year,
+                    "monthlyincome",
+                    month=job_dispatcher_test_date.month,
+                    cpr=None,
+                    skew=True,
+                    traceback=False,
+                    reraise=False,
+                    verbosity=1,
+                ),
+                call(
+                    ManagementCommands.LOAD_ESKAT,
+                    job_dispatcher_test_date.year,
+                    "taxinformation",
+                    month=job_dispatcher_test_date.month,
+                    cpr=None,
+                    skew=False,
+                    traceback=False,
+                    reraise=False,
+                    verbosity=1,
+                ),
+                call(
+                    ManagementCommands.LOAD_PRISME_B_TAX, traceback=False, reraise=False
+                ),
+                call(
+                    ManagementCommands.IMPORT_U1A_DATA,
+                    year=job_dispatcher_test_date.year,
+                    cpr=None,
+                    verbosity=1,
+                    traceback=False,
+                    reraise=False,
+                ),
+                call(
+                    ManagementCommands.GET_PERSON_INFO_FROM_DAFO,
+                    cpr=None,
+                    verbosity=1,
+                    traceback=False,
+                    reraise=False,
+                ),
+                # Estimation-jobs
+                call(
+                    ManagementCommands.ESTIMATE_INCOME,
+                    year=job_dispatcher_test_date.year,
+                    cpr=None,
+                    verbosity=1,
+                    traceback=False,
+                    reraise=False,
+                ),
+            ]
+        )
