af7f6ebc2267a57b4e71709b02ac9ba98cc3226c
Thor Astrup Pedersen <tap@magenta-aps.dk>
2025-05-08 13:23:25 +0200

    refactor: [#64642] GET_PERSON_INFO_FROM_DAFO don't depend on LOAD_ESKAT in the JobDispatcher anymore

--- suila/suila/dispatch.py
+++ suila/suila/dispatch.py
@@ -33,9 +33,7 @@ class JobDispatcher:
             ManagementCommands.LOAD_ESKAT: [],
             ManagementCommands.LOAD_PRISME_B_TAX: [],
             ManagementCommands.IMPORT_U1A_DATA: [],
-            ManagementCommands.GET_PERSON_INFO_FROM_DAFO: [
-                ManagementCommands.LOAD_ESKAT,
-            ],
+            ManagementCommands.GET_PERSON_INFO_FROM_DAFO: [],
             ManagementCommands.ESTIMATE_INCOME: [
                 ManagementCommands.LOAD_ESKAT,
             ],
