5f896ae378ae440945495dbf934296a13b101ed1
Nick Janssen <nj@magenta-aps.dk>
2025-05-15 09:52:05 +0200

    fix: [#64046] Better output for autoselect command and fix input params
    `year` is a mandatory input parameter but later on in the code we check
    if year is supplied and select all years for a person if it is not.
    
    Also added print statements so we know how much time is remaining

--- suila/suila/tests/test_autoselect.py
+++ suila/suila/tests/test_autoselect.py
@@ -18,12 +18,21 @@ class AutoSelectEngineTests(TestCase):
         cls.year_2022 = Year.objects.create(year=2022)
         cls.year_2023 = Year.objects.create(year=2023)
         cls.person = Person.objects.create(cpr="123")
+        cls.person2 = Person.objects.create(cpr="1234")
         cls.person_year_2022 = PersonYear.objects.create(
             year=cls.year_2022, person=cls.person
         )
         cls.person_year_2023 = PersonYear.objects.create(
             year=cls.year_2023, person=cls.person
         )
+
+        cls.person2_year_2022 = PersonYear.objects.create(
+            year=cls.year_2022, person=cls.person2
+        )
+        cls.person2_year_2023 = PersonYear.objects.create(
+            year=cls.year_2023, person=cls.person2
+        )
+
         PersonYearEstimateSummary.objects.create(
             person_year=cls.person_year_2022,
             estimation_engine="InYearExtrapolationEngine",
@@ -54,12 +63,9 @@ class AutoSelectEngineTests(TestCase):
         )
 
     def call_command(self, *args, **kwargs):
-        out = StringIO()
         call_command(
             "autoselect_estimation_engine",
             *args,
-            stdout=out,
-            stderr=StringIO(),
             **kwargs,
         )
 
@@ -92,34 +98,52 @@ class AutoSelectEngineTests(TestCase):
         self.assert_that_engines_equal_defaults()
 
         # Now run the autoselect command
-        self.call_command(2023)
+        self.call_command(year=2023)
 
         # See. Now the engines are updated for 2023: To those that were best in 2022
         self.assert_that_engines_are_updated()
 
     def test_autoselect_cpr_arg(self):
         # Call the command for a different person
-        self.call_command(2023, cpr="0011")
+        self.call_command(year=2023, cpr="0011")
 
         # The engine still equals the default engine for person with cpr = 123
         self.assert_that_engines_equal_defaults()
 
     def test_autoselect_for_all_years(self):
-        self.call_command(0)
+        self.call_command()
         self.assert_that_engines_are_updated()
 
     def test_autoselect_for_empty_year(self):
-        self.call_command(2022)
+        self.call_command(year=2022)
         self.assert_that_engines_equal_defaults()
 
     @patch("suila.management.commands.autoselect_estimation_engine.PersonYear.objects")
     def test_autoselect_for_nonexising_person_year(self, person_year_mock):
         person_year_mock.get.side_effect = PersonYear.DoesNotExist
-        self.call_command(2023)
+        self.call_command(year=2023)
         self.assert_that_engines_equal_defaults()
 
     @patch("suila.management.commands.autoselect_estimation_engine.Profile")
     def test_autoselect_management_command_profiler(self, profiler):
         self.assertFalse(profiler.called)
-        self.call_command(2023, profile=True)
+        self.call_command(year=2023, profile=True)
         self.assertTrue(profiler.called)
+
+    def test_verbose(self):
+        stdout = StringIO()
+        self.call_command(verbosity=3, stdout=stdout)
+        self.assertIn("Processing person 1", stdout.getvalue())
+
+    def test_no_years_to_process(self):
+        # There is no year before 2022. So if we  call with year=2022 there are no
+        # years to process
+        stdout = StringIO()
+        self.call_command(year=2022, verbosity=3, stdout=stdout)
+        self.assertIn("No relevant years found", stdout.getvalue())
+
+    def test_no_relevant_summaries(self):
+        # Person 1234 has no summaries
+        stdout = StringIO()
+        self.call_command(year=2023, cpr="1234", stdout=stdout, verbosity=3)
+        self.assertIn("No relevant PersonYearEstimateSummaries", stdout.getvalue())
