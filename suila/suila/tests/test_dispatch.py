9940b37428832db0fcd7918d561157f354d99838
Thor Astrup Pedersen <tap@magenta-aps.dk>
2025-05-23 15:29:27 +0200

    test: [#64642] removed deprecated tests: test_allow_job & test_job_ran_this_month

--- suila/suila/tests/test_dispatch.py
+++ suila/suila/tests/test_dispatch.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: MPL-2.0
 
 import datetime
-import random
 from unittest import mock
 from unittest.mock import MagicMock
 
@@ -42,28 +41,6 @@ class TestJobDispatcher(TestCase):
         )
 
         cls.job_dispatcher = JobDispatcher(year=2025, month=1, day=1)
-
-    def test_job_ran_this_month(self):
-        with self.subTest("current month"):
-            self.assertTrue(
-                self.job_dispatcher.job_ran_this_month(
-                    ManagementCommands.CALCULATE_STABILITY_SCORE
-                )
-            )
-            self.assertFalse(
-                self.job_dispatcher.job_ran_this_month(ManagementCommands.LOAD_ESKAT)
-            )
-
-        with self.subTest("next month"):
-            next_month = (datetime.datetime.today().month + 1) % 12
-            future_job_dispatcher = JobDispatcher(
-                year=self.job_dispatcher.year, month=next_month, day=1
-            )
-            self.assertFalse(
-                future_job_dispatcher.job_ran_this_month(
-                    ManagementCommands.CALCULATE_STABILITY_SCORE
-                )
-            )
 
     def test_job_ran_this_year(self):
         self.assertTrue(
@@ -127,71 +104,3 @@ class TestJobDispatcher(TestCase):
         job_dispatcher.job_ran_this_month.return_value = job_ran_this_month
 
         return job_dispatcher.allow_job(name)
-
-    def test_allow_job(self):
-        self.assertTrue(
-            self.allow_job(ManagementCommands.CALCULATE_STABILITY_SCORE, 2021, 3, 1)
-        )
-        self.assertFalse(
-            self.allow_job(ManagementCommands.CALCULATE_STABILITY_SCORE, 2021, 2, 1)
-        )
-
-        self.assertTrue(
-            self.allow_job(
-                ManagementCommands.LOAD_ESKAT,
-                random.randint(2000, 3000),
-                random.randint(1, 12),
-                random.randint(1, 31),
-            )
-        )
-        self.assertTrue(
-            self.allow_job(
-                ManagementCommands.ESTIMATE_INCOME,
-                random.randint(2000, 3000),
-                random.randint(1, 12),
-                random.randint(1, 31),
-            )
-        )
-
-        # 2024-01-08 is "the day before the second tuesday" in january
-        self.assertTrue(
-            self.allow_job(ManagementCommands.CALCULATE_BENEFIT, 2024, 1, 8)
-        )
-        self.assertFalse(
-            self.allow_job(ManagementCommands.CALCULATE_BENEFIT, 2024, 1, 7)
-        )
-
-        self.assertTrue(
-            self.allow_job(ManagementCommands.CALCULATE_BENEFIT, 2024, 1, 14)
-        )
-        self.assertFalse(
-            self.allow_job(ManagementCommands.CALCULATE_BENEFIT, 2024, 1, 15)
-        )
-
-        # 2024-01-15 is the day before the third tuesday in january
-        self.assertTrue(
-            self.allow_job(ManagementCommands.EXPORT_BENEFITS_TO_PRISME, 2024, 1, 15)
-        )
-        self.assertFalse(
-            self.allow_job(ManagementCommands.EXPORT_BENEFITS_TO_PRISME, 2024, 1, 14)
-        )
-
-        # If the job already ran it should not be run again
-        self.assertFalse(
-            self.allow_job(
-                ManagementCommands.EXPORT_BENEFITS_TO_PRISME,
-                2024,
-                1,
-                15,
-                job_ran_this_month=True,
-            )
-        )
-        self.assertFalse(
-            self.allow_job(
-                ManagementCommands.CALCULATE_BENEFIT,
-                2024,
-                1,
-                9,
-                job_ran_this_month=True,
-            )
-        )
