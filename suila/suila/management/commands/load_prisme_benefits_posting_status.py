902226f0b97b6fcc22447568fc607a1ff988d38f
Mads Riis-Weitling <mrw@magenta.dk>
2025-05-26 11:27:39 +0200

    feat: [#64795] Incorporate lookup by CPR/date/amount into main logic
    This removes the import class `PostingStatusImportMissingInvoiceNumber`
    and incorporates its logic into the main import class
    `PostingStatusImport`.
    
    This is done to ensure that importing posting status by CPR/date/amount
    follows the same rules as the normal import by invoice number, i.e.:
    - files are processed chronologically
    - each file import can only touch Prisme batch items older than the "max
    date" found in the file
    - if a Prisme batch item is matched by CPR/date/amount, its status can
    be moved from "failed" to "posted" in the same manner as normal matches
    done by invoice number.

--- suila/suila/management/commands/load_prisme_benefits_posting_status.py
+++ suila/suila/management/commands/load_prisme_benefits_posting_status.py
@@ -3,28 +3,12 @@
 # SPDX-License-Identifier: MPL-2.0
 from django.core.management.base import BaseCommand
 
-from suila.integrations.prisme.posting_status import (
-    PostingStatusImport,
-    PostingStatusImportMissingInvoiceNumber,
-)
+from suila.integrations.prisme.posting_status import PostingStatusImport
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--unknown-invoice-number",
-            action="store_true",
-            help="Process posting status for unknown invoice numbers",
-        )
-
     def handle(self, *args, **options):
-        if options["unknown_invoice_number"]:
-            posting_status_import: PostingStatusImportMissingInvoiceNumber = (
-                PostingStatusImportMissingInvoiceNumber()
-            )
-        else:
-            posting_status_import: PostingStatusImport = PostingStatusImport()
-
+        posting_status_import: PostingStatusImport = PostingStatusImport()
         posting_status_import.import_posting_status(
             self.stdout, verbosity=options["verbosity"]
         )
