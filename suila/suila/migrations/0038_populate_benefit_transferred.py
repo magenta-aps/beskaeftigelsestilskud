9cec20d3b2ff47b006602f8082be1183b1b76c80
Nick Janssen <nj@magenta-aps.dk>
2025-05-28 09:11:57 +0200

    fix: [#65182] Improve performance of migration
    This migration was too heavy for the test server. We add logger print
    statements and improve the database logic

--- suila/suila/migrations/0038_populate_benefit_transferred.py
+++ suila/suila/migrations/0038_populate_benefit_transferred.py
@@ -1,42 +1,61 @@
 # Ceated by Nick on 2025-05-26
 from django.db import migrations
-from django.db.models import Sum
 from common.model_utils import get_amount_from_g68_content
+import logging
+from django.db import transaction
+from django.db.models import Prefetch
+
+logger = logging.getLogger(__name__)
 
 
 def populate_benefit_transferred(apps, schema_editor):
-
     model = apps.get_model("suila", "PersonMonth")
+    logger.info("Populating person_month.benefit_transferred")
 
-    for person_month in model.objects.all():
-        if hasattr(person_month, "prismebatchitem"):
+    person_months = model.objects.filter(
+        person_year__year__year=2025,
+        prismebatchitem__isnull=False,
+    ).select_related("prismebatchitem")
+
+    total_person_months = person_months.count()
+
+    with transaction.atomic():
+        for counter, person_month in enumerate(person_months, start=1):
+            logger.info(f"Processing person_month {counter}/{total_person_months}")
 
             # We use the get_amount_from_g68_content function because using a
             # property (prismebatchitem.amount) is not allowed in a migration
             person_month.benefit_transferred = get_amount_from_g68_content(
                 person_month.prismebatchitem.g68_content
             )
-            person_month.save()
+            person_month.save(update_fields=["benefit_transferred"])
 
 
 def populate_prior_benefit_transferred(apps, schema_editor):
+    PersonYear = apps.get_model("suila", "PersonYear")
+    PersonMonth = apps.get_model("suila", "PersonMonth")
+    logger.info("Populating person_month.prior_benefit_transferred")
 
-    model = apps.get_model("suila", "PersonMonth")
-    for person_month in model.objects.all():
+    # Prefetch related PersonMonth objects, ordered by month
+    person_years = PersonYear.objects.filter(year__year=2025).prefetch_related(
+        Prefetch(
+            "personmonth_set",
+            queryset=PersonMonth.objects.order_by("month"),
+        )
+    )
+    total_person_years = person_years.count()
 
-        month = person_month.month
-        year = person_month.person_year.year.year
-        person_pk = person_month.person_year.person.pk
+    with transaction.atomic():
+        for counter, person_year in enumerate(person_years, start=1):
+            logger.info(f"Processing person_year {counter}/{total_person_years}")
 
-        prior_benefit_transferred = model.objects.filter(
-            month__lt=month,
-            person_year__year__year=year,
-            person_year__person__pk=person_pk,
-        ).aggregate(total=Sum("benefit_transferred"))["total"]
+            prior_benefit_transferred = 0
+            for person_month in person_year.personmonth_set.all():
+                if prior_benefit_transferred:
+                    person_month.prior_benefit_transferred = prior_benefit_transferred
+                    person_month.save(update_fields=["prior_benefit_transferred"])
 
-        if prior_benefit_transferred:
-            person_month.prior_benefit_transferred = prior_benefit_transferred
-            person_month.save()
+                prior_benefit_transferred += person_month.benefit_transferred or 0
 
 
 class Migration(migrations.Migration):
--- suila/suila/tests/test_migrations.py
+++ suila/suila/tests/test_migrations.py
@@ -23,77 +23,82 @@ class PopulateBenefitTransferredTest(MigratorTestCase):
         PrismeBatch = self.old_state.apps.get_model("suila", "PrismeBatch")
         PrismeBatchItem = self.old_state.apps.get_model("suila", "PrismeBatchItem")
 
-        self.year = Year.objects.create(year=2024)
+        self.year = Year.objects.create(year=2025)
+        self.last_year = Year.objects.create(year=2024)
 
         # A person with 2 payouts. One in January and one in February.
         self.person = Person.objects.create(name="Jens Hansen", cpr="1234567890")
-        self.person_year = PersonYear.objects.create(
-            person=self.person,
-            year=self.year,
-            preferred_estimation_engine_a="InYearExtrapolationEngine",
-        )
-
-        self.person_month1 = PersonMonth.objects.create(
-            person_year=self.person_year, month=1, import_date=date.today()
-        )
-        self.person_month2 = PersonMonth.objects.create(
-            person_year=self.person_year, month=2, import_date=date.today()
-        )
-        self.person_month3 = PersonMonth.objects.create(
-            person_year=self.person_year, month=3, import_date=date.today()
-        )
-
-        self.prisme_batch = PrismeBatch.objects.create(
-            status="sent", export_date=date.today(), prefix=1
-        )
-
-        # Month 1 has a prisme-batch item
-        self.prisme_item1 = PrismeBatchItem.objects.create(
-            person_month=self.person_month1,
-            prisme_batch=self.prisme_batch,
-            g68_content=(
-                "000G6800004011&020900&0300&"
-                "07000000000000000000&0800000031700&"  # 317 kr.
-                "09+&1002&1100000101001111&1220250414&"
-                "16202504080080400004&"
-                "1700000000000027100004&40www.suila.gl takuuk"
-            ),
-        )
-
-        # Month 2 does not have a prisme-batch item
-        self.prisme_item2 = PrismeBatchItem.objects.create(
-            person_month=self.person_month2,
-            prisme_batch=self.prisme_batch,
-            g68_content=(
-                "000G6800004011&020900&0300&"
-                "07000000000000000000&0800000031800&"  # 318 kr.
-                "09+&1002&1100000101001111&1220250414&"
-                "16202504080080400004&"
-                "1700000000000027100004&40www.suila.gl takuuk"
-            ),
-        )
 
         # A person without payouts
         self.person2 = Person.objects.create(name="Jakob Hansen", cpr="1234567891")
-        self.person_year2 = PersonYear.objects.create(
-            person=self.person2,
-            year=self.year,
-            preferred_estimation_engine_a="InYearExtrapolationEngine",
-        )
 
-        PersonMonth.objects.create(
-            person_year=self.person_year2, month=1, import_date=date.today()
-        )
-        PersonMonth.objects.create(
-            person_year=self.person_year2, month=2, import_date=date.today()
-        )
-        PersonMonth.objects.create(
-            person_year=self.person_year2, month=3, import_date=date.today()
-        )
+        for year in [self.year, self.last_year]:
 
-    def get_person_month(self, month, name):
+            self.person_year = PersonYear.objects.create(
+                person=self.person,
+                year=year,
+                preferred_estimation_engine_a="InYearExtrapolationEngine",
+            )
+
+            self.person_month1 = PersonMonth.objects.create(
+                person_year=self.person_year, month=1, import_date=date.today()
+            )
+            self.person_month2 = PersonMonth.objects.create(
+                person_year=self.person_year, month=2, import_date=date.today()
+            )
+            self.person_month3 = PersonMonth.objects.create(
+                person_year=self.person_year, month=3, import_date=date.today()
+            )
+
+            self.prisme_batch = PrismeBatch.objects.create(
+                status="sent", export_date=date.today(), prefix=1
+            )
+
+            # Month 1 has a prisme-batch item
+            self.prisme_item1 = PrismeBatchItem.objects.create(
+                person_month=self.person_month1,
+                prisme_batch=self.prisme_batch,
+                g68_content=(
+                    "000G6800004011&020900&0300&"
+                    "07000000000000000000&0800000031700&"  # 317 kr.
+                    "09+&1002&1100000101001111&1220250414&"
+                    "16202504080080400004&"
+                    "1700000000000027100004&40www.suila.gl takuuk"
+                ),
+            )
+
+            # Month 2 does not have a prisme-batch item
+            self.prisme_item2 = PrismeBatchItem.objects.create(
+                person_month=self.person_month2,
+                prisme_batch=self.prisme_batch,
+                g68_content=(
+                    "000G6800004011&020900&0300&"
+                    "07000000000000000000&0800000031800&"  # 318 kr.
+                    "09+&1002&1100000101001111&1220250414&"
+                    "16202504080080400004&"
+                    "1700000000000027100004&40www.suila.gl takuuk"
+                ),
+            )
+
+            self.person_year2 = PersonYear.objects.create(
+                person=self.person2,
+                year=year,
+                preferred_estimation_engine_a="InYearExtrapolationEngine",
+            )
+
+            PersonMonth.objects.create(
+                person_year=self.person_year2, month=1, import_date=date.today()
+            )
+            PersonMonth.objects.create(
+                person_year=self.person_year2, month=2, import_date=date.today()
+            )
+            PersonMonth.objects.create(
+                person_year=self.person_year2, month=3, import_date=date.today()
+            )
+
+    def get_person_month(self, month, name, year=2025):
         return self.new_state.apps.get_model("suila", "PersonMonth").objects.get(
-            month=month, person_year__person__name=name
+            month=month, person_year__person__name=name, person_year__year__year=year
         )
 
     def test_migration(self):
@@ -112,6 +117,18 @@ class PopulateBenefitTransferredTest(MigratorTestCase):
         person_month1 = self.get_person_month(1, "Jakob Hansen")
         person_month2 = self.get_person_month(2, "Jakob Hansen")
         person_month3 = self.get_person_month(3, "Jakob Hansen")
+
+        self.assertEqual(person_month1.benefit_transferred, 0)
+        self.assertEqual(person_month2.benefit_transferred, 0)
+        self.assertEqual(person_month3.benefit_transferred, 0)
+        self.assertEqual(person_month1.prior_benefit_transferred, None)
+        self.assertEqual(person_month2.prior_benefit_transferred, None)
+        self.assertEqual(person_month3.prior_benefit_transferred, None)
+
+    def test_that_previous_years_are_not_processed(self):
+        person_month1 = self.get_person_month(1, "Jens Hansen", 2024)
+        person_month2 = self.get_person_month(2, "Jens Hansen", 2024)
+        person_month3 = self.get_person_month(3, "Jens Hansen", 2024)
 
         self.assertEqual(person_month1.benefit_transferred, 0)
         self.assertEqual(person_month2.benefit_transferred, 0)
