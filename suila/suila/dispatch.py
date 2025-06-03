d8ec2b2a987a740850451dd2758de80060ede418
Thor Astrup Pedersen <tap@magenta-aps.dk>
2025-06-03 14:00:31 +0200

    feat: [#64642] new get_eboks_date + EBOKS_DATE_PAYOUT_DATE_OFFSET_DAYS setting

--- suila/suila/dispatch.py
+++ suila/suila/dispatch.py
@@ -3,13 +3,13 @@
 # SPDX-License-Identifier: MPL-2.0
 import datetime
 import logging
-from typing import Optional
+from typing import Dict, Optional
 
 from django.core import management
 from django.db.models import Q
 from django.utils import timezone
 
-from suila.benefit import get_calculation_date, get_prisme_date
+from suila.benefit import get_calculation_date, get_eboks_date, get_prisme_date
 from suila.exceptions import DependenciesNotMet
 from suila.models import JobLog, ManagementCommands, StatusChoices
 
@@ -17,15 +17,93 @@ logger = logging.getLogger(__name__)
 
 
 class JobDispatcher:
+    JOB_TYPE_YEARLY = "yearly"
+    JOB_TYPE_MONTHLY = "monthly"
+
+    jobs = {
+        # "year"-Jobs
+        ManagementCommands.CALCULATE_STABILITY_SCORE: {
+            "type": "yearly",
+            "validator": lambda year, month, day: (
+                # Allowed to run the day after SEND_EBOKS in february
+                month == 2
+                and day >= get_eboks_date(year, month).day + 1
+            ),
+        },
+        ManagementCommands.AUTOSELECT_ESTIMATION_ENGINE: {
+            "type": "yearly",
+            "validator": lambda year, month, day: (
+                # Allowed to run the day after SEND_EBOKS in february
+                month == 2
+                and day >= get_eboks_date(year, month).day + 1
+            ),
+        },
+        # "load"-jobs
+        ManagementCommands.LOAD_ESKAT: {
+            "type": "monthly",
+            "validator": lambda year, month, day: (
+                day >= get_calculation_date(year, month).day
+            ),
+        },
+        ManagementCommands.LOAD_PRISME_B_TAX: {
+            "type": "monthly",
+            "validator": lambda year, month, day: (
+                day >= get_calculation_date(year, month).day
+            ),
+        },
+        ManagementCommands.IMPORT_U1A_DATA: {
+            "type": "monthly",
+            "validator": lambda year, month, day: (
+                day >= get_calculation_date(year, month).day
+            ),
+        },
+        ManagementCommands.GET_PERSON_INFO_FROM_DAFO: {
+            "type": "monthly",
+            "validator": lambda year, month, day: (
+                day >= get_calculation_date(year, month).day
+            ),
+        },
+        # "estimation"-jobs
+        ManagementCommands.ESTIMATE_INCOME: {
+            "type": "monthly",
+            "validator": lambda year, month, day: (
+                day >= get_calculation_date(year, month).day
+            ),
+        },
+        # "calculation"-jobs
+        ManagementCommands.CALCULATE_BENEFIT: {
+            "type": "monthly",
+            "validator": lambda year, month, day: (
+                get_calculation_date(year, month).day
+                <= day
+                < get_prisme_date(year, month).day
+            ),
+        },
+        # "export"-jobs
+        ManagementCommands.EXPORT_BENEFITS_TO_PRISME: {
+            "type": "monthly",
+            "validator": lambda year, month, day: (
+                day >= get_prisme_date(year, month).day
+            ),
+        },
+        ManagementCommands.SEND_EBOKS: {
+            "type": "monthly",
+            "validator": lambda year, month, day: (
+                day >= get_eboks_date(year, month).day
+            ),
+        },
+    }
+
     def __init__(self, day=None, month=None, year=None, reraise=False):
-        today = timezone.now()
-        self.year = year or today.year
-        self.month = month or today.month
-        self.day = day or today.day
+        self.now = timezone.now()
+        self.year = year or self.now.year
+        self.month = month or self.now.month
+        self.day = day or self.now.day
         self.reraise = reraise
 
         self.calculation_date = get_calculation_date(self.year, self.month)
         self.prisme_date = get_prisme_date(self.year, self.month)
+        self.eboks_date = get_eboks_date(self.year, self.month)
 
         self.dependencies = {
             ManagementCommands.CALCULATE_STABILITY_SCORE: [],
@@ -33,9 +111,7 @@ class JobDispatcher:
             ManagementCommands.LOAD_ESKAT: [],
             ManagementCommands.LOAD_PRISME_B_TAX: [],
             ManagementCommands.IMPORT_U1A_DATA: [],
-            ManagementCommands.GET_PERSON_INFO_FROM_DAFO: [
-                ManagementCommands.LOAD_ESKAT,
-            ],
+            ManagementCommands.GET_PERSON_INFO_FROM_DAFO: [],
             ManagementCommands.ESTIMATE_INCOME: [
                 ManagementCommands.LOAD_ESKAT,
             ],
@@ -54,107 +130,109 @@ class JobDispatcher:
             ManagementCommands.LOAD_PRISME_BENEFITS_POSTING_STATUS: [],
         }
 
-    def job_ran_this_month(self, name):
-        return self.job_ran_month(name, self.month)
+    def job_ran_month(
+        self,
+        name: str,
+        year: int,
+        month: int,
+        job_params: Optional[Dict[str, str]] = None,
+    ):
+        filters_kwargs = {
+            "name": name,
+            "status": StatusChoices.SUCCEEDED,
+        }
 
-    def job_ran_month(self, name: str, month: int):
+        if job_params:
+            for job_param_name, job_param_value in job_params.items():
+                if not job_param_name.endswith("_param"):
+                    continue
+                filters_kwargs[job_param_name] = job_param_value
+
         return JobLog.objects.filter(
-            Q(month_param=month) | Q(month_param=None),
-            name=name,
-            status=StatusChoices.SUCCEEDED,
-            year_param=self.year,
+            Q(runtime__year=year), Q(runtime__month=month), **filters_kwargs
         ).exists()
 
-    def job_ran_this_year(self, name):
-        return self.job_ran_year(name, self.year)
+    def job_ran_year(
+        self, name: str, year: int, job_params: Optional[Dict[str, str]] = None
+    ):
+        filters_kwargs = {
+            "name": name,
+            "status": StatusChoices.SUCCEEDED,
+        }
 
-    def job_ran_year(self, name: str, year: int):
-        return JobLog.objects.filter(
-            name=name,
-            status=StatusChoices.SUCCEEDED,
-            year_param=year,
-        ).exists()
+        if job_params:
+            for job_param_name, job_param_value in job_params.items():
+                if not job_param_name.endswith("_param"):
+                    continue
+                filters_kwargs[job_param_name] = job_param_value
+
+        return JobLog.objects.filter(Q(runtime__year=year), **filters_kwargs).exists()
 
     def check_dependencies(self, name):
-        dependencies = self.dependencies[name]
-
-        for dependency in dependencies:
-            job_ran_this_month = self.job_ran_this_month(dependency)
-            if not job_ran_this_month:
+        for dependency in self.dependencies[name]:
+            if not self.job_ran_month(dependency, self.year, self.month):
                 raise DependenciesNotMet(name, dependency)
 
-    def allow_job(
-        self, name, year: Optional[int] = None, month: Optional[int] = None
-    ) -> bool:
+    def allow_job(self, name, job_params: Optional[Dict[str, str]] = None) -> bool:
         """
         Determine whether to run a job or not
         """
-        job_ran_this_year = (
-            self.job_ran_this_year(name) if not year else self.job_ran_year(name, year)
-        )
-        job_ran_this_month = (
-            self.job_ran_this_month(name)
-            if not month
-            else self.job_ran_month(name, month)
-        )
+        if name not in self.jobs:
+            return False
 
-        if name in [
-            ManagementCommands.CALCULATE_STABILITY_SCORE,
-            ManagementCommands.AUTOSELECT_ESTIMATION_ENGINE,
-        ]:
-            if (
-                self.month == 3
-                and self.day < self.calculation_date.day
-                and not job_ran_this_year
-            ):
-                return True
+        # Handle job based on type
+        job_config = self.jobs[name]
+        match (job_config["type"]):
+            case self.JOB_TYPE_YEARLY:
+                if self.job_ran_year(name, self.year, job_params):
+                    return False
+                return job_config["validator"](  # type: ignore[operator]
+                    self.year, self.month, self.day
+                )
+            case self.JOB_TYPE_MONTHLY:
+                if self.job_ran_month(name, self.year, self.month, job_params):
+                    return False
+                return job_config["validator"](  # type: ignore[operator]
+                    self.year, self.month, self.day
+                )
 
-        elif name in [
-            ManagementCommands.LOAD_ESKAT,
-            ManagementCommands.LOAD_PRISME_B_TAX,
-            ManagementCommands.IMPORT_U1A_DATA,
-            ManagementCommands.GET_PERSON_INFO_FROM_DAFO,
-            ManagementCommands.ESTIMATE_INCOME,
-        ]:
-            return True
-
-        elif name == ManagementCommands.CALCULATE_BENEFIT:
-            if (
-                self.calculation_date.day <= self.day < self.prisme_date.day
-                and not job_ran_this_month
-            ):
-                return True
-
-        elif name == ManagementCommands.EXPORT_BENEFITS_TO_PRISME:  # pragma: no branch
-            if self.day >= self.prisme_date.day and not job_ran_this_month:
-                return True
-
-        elif name == ManagementCommands.LOAD_PRISME_BENEFITS_POSTING_STATUS:
-            return True
-
+        # Default to "False" if the job-type is inknown
+        logger.error(f'error: job {name} have an unknown "type": {job_config["type"]}')
         return False
 
     def call_job(self, name, *args, **kwargs):
-        allow_job_kwargs = {}
-        if name in [
-            ManagementCommands.CALCULATE_STABILITY_SCORE,
-        ]:
-            allow_job_kwargs["year"] = args[0]
+        # Gather job_params
+        job_params = {}
+        if "year" in kwargs:
+            job_params["year_param"] = kwargs["year"]
+        if "month" in kwargs:
+            job_params["month_param"] = kwargs["month"]
 
-        if not self.allow_job(name, **allow_job_kwargs):
+        match (name):
+            case ManagementCommands.CALCULATE_STABILITY_SCORE:
+                job_params["year_param"] = args[0]
+            case ManagementCommands.LOAD_ESKAT:
+                job_params["type_param"] = args[1]
+
+        # Check if the job is allowed to be called
+        if not self.allow_job(name, job_params):
             return
 
-        self.check_dependencies(name)
+        # If allowed, check if job dependencies are met
+        try:
+            self.check_dependencies(name)
+        except DependenciesNotMet:
+            logger.warning(f"DependencyNotMet exception for job: {name}")
+        else:
+            logger.info(
+                f"\n{datetime.date(self.year, self.month, self.day)}: "
+                f"Running job {name} ..."
+            )
 
-        logger.info(
-            f"\n{datetime.date(self.year, self.month, self.day)}: "
-            f"Running job {name} ..."
-        )
-
-        management.call_command(
-            name,
-            *args,
-            traceback=self.reraise,
-            reraise=self.reraise,
-            **kwargs,
-        )
+            management.call_command(
+                name,
+                *args,
+                traceback=self.reraise,
+                reraise=self.reraise,
+                **kwargs,
+            )
