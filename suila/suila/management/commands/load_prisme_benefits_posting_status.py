f97340ec14fb2eedb53bcc1034627f1aea9e1d09
Nick Janssen <nj@magenta-aps.dk>
2025-06-10 11:24:42 +0200

    fix: [#65337] load_prisme_benefits_posting_status now appears in the job log

--- suila/suila/management/commands/load_prisme_benefits_posting_status.py
+++ suila/suila/management/commands/load_prisme_benefits_posting_status.py
@@ -1,13 +1,14 @@
 # SPDX-FileCopyrightText: 2024 Magenta ApS <info@magenta.dk>
 #
 # SPDX-License-Identifier: MPL-2.0
-from django.core.management.base import BaseCommand
-
 from suila.integrations.prisme.posting_status import PostingStatusImport
+from suila.management.commands.common import SuilaBaseCommand
 
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
+class Command(SuilaBaseCommand):
+    filename = __file__
+
+    def _handle(self, *args, **options):
         posting_status_import: PostingStatusImport = PostingStatusImport()
         posting_status_import.import_posting_status(
             self.stdout, verbosity=options["verbosity"]
