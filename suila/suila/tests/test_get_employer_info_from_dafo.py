cd84c5ea3b1da12f26552d7b82b5e9f8da2b4708
Nick Janssen <nj@magenta-aps.dk>
2025-06-24 08:42:35 +0200

    test: [#64646] Test get_employer_info_from_dafo

--- suila/suila/tests/test_get_employer_info_from_dafo.py
+++ suila/suila/tests/test_get_employer_info_from_dafo.py
@@ -1,15 +1,14 @@
 # SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
 #
 # SPDX-License-Identifier: MPL-2.0
-from unittest.mock import Mock, patch
+from io import StringIO
+from unittest.mock import MagicMock, patch
 
+from django.core.management import call_command
 from django.test import TestCase
 from django.test.utils import override_settings
+from requests.exceptions import HTTPError
 
-from suila.integrations.pitu.client import PituClient
-from suila.management.commands.get_employer_info_from_dafo import (
-    Command as GetEmployerInfoFromDafoCommand,
-)
 from suila.models import Employer
 
 
@@ -46,6 +45,15 @@ class TestGetPersonInfoFromDafoCommand(TestCase):
         "cvr_service": "test_cvr_service",
     }
 
+    def setUp(self):
+        self.pitu_client_patcher = patch(
+            "suila.management.commands.get_employer_info_from_dafo.PituClient"
+        )
+        self.pitu_client_mock = self.pitu_client_patcher.start()
+        self.client_mock = MagicMock()
+        self.client_mock.get.return_value = self._mock_result
+        self.pitu_client_mock.return_value = self.client_mock
+
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
@@ -72,18 +80,45 @@ class TestGetPersonInfoFromDafoCommand(TestCase):
 
     @override_settings(PITU=_mock_pitu_settings)
     def test_pitu_client_initialization(self):
-        command = GetEmployerInfoFromDafoCommand()
-        client = command._get_pitu_client()
-        self.assertIsInstance(client, PituClient)
-        self.assertIn("cvr", client.service)
+        self._run()
+        self.pitu_client_mock.assert_called_with(
+            base_url="test_url",
+            service="test_cvr_service",
+            client_header="test_header",
+            certificate="test_cert",
+            private_key="test_key",
+            root_ca="test_ca",
+        )
+
+    def test_no_employers(self):
+        stdout = StringIO()
+        self._run(cvr="1234", stdout=stdout, verbosity=3)
+        self.assertIn("Could not find any employers", stdout.getvalue())
+
+    def test_http_404(self):
+        stdout = StringIO()
+        response = MagicMock(status_code=404)
+        self.client_mock.get.side_effect = HTTPError(response=response)
+        self.pitu_client_mock.return_value = self.client_mock
+        self._run(stdout=stdout, verbosity=3)
+        self.assertIn("Could not find employer", stdout.getvalue())
+
+    def test_unexpected_http_error(self):
+        stdout = StringIO()
+        response = MagicMock(status_code=303)
+        self.client_mock.get.side_effect = HTTPError(response=response)
+        self.pitu_client_mock.return_value = self.client_mock
+        self._run(stdout=stdout, verbosity=3)
+        self.assertIn("Unexpected 303 error", stdout.getvalue())
+
+    def test_no_name(self):
+        stdout = StringIO()
+        mock_result = self._mock_result
+        mock_result.pop("navn")
+        self.client_mock.get.return_value = self._mock_result
+        self.pitu_client_mock.return_value = self.client_mock
+        self._run(stdout=stdout, verbosity=3)
+        self.assertIn("No employer name", stdout.getvalue())
 
     def _run(self, **kwargs):
-        # Arrange
-        kwargs.setdefault("verbosity", 0)
-        kwargs.setdefault("cvr", None)
-        command = GetEmployerInfoFromDafoCommand()
-        mock_client = Mock()
-        mock_client.get.return_value = self._mock_result
-        with patch.object(command, "_get_pitu_client", return_value=mock_client):
-            # Act
-            command._handle(**kwargs)
+        call_command("get_employer_info_from_dafo", **kwargs)
