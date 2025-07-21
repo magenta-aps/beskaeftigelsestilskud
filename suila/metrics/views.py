0d9fc1c44941067f5c950216a1c6e9f7f391d6f8
Mads Riis-Weitling <mrw@magenta-aps.dk>
2025-07-21 16:15:44 +0200

    chore: [#64795] Import and install `metrics` app

---
+++ suila/metrics/views.py
@@ -0,0 +1,56 @@
+# SPDX-FileCopyrightText: 2025 Magenta ApS <info@magenta.dk>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+import logging
+import tempfile
+
+from django.conf import settings
+from django.db import connection
+from django.http import HttpResponse
+from tenQ.client import ClientException, list_prisme_folder
+
+logger = logging.getLogger(__name__)
+
+
+def health_check_storage(request):
+    try:
+        with tempfile.NamedTemporaryFile(
+            dir=settings.MEDIA_ROOT, delete=True
+        ) as temp_file:
+            test_content = b"Test"
+            temp_file.write(test_content)
+            temp_file.flush()
+            temp_file.seek(0)
+
+            content = temp_file.read()
+            if content != test_content:
+                raise ValueError(
+                    (
+                        "File content does not match, when trying to write and "
+                        "read from file"
+                    )
+                )
+
+        return HttpResponse("OK")
+    except Exception:
+        logger.exception("Storage health check failed")
+        return HttpResponse("ERROR", status=500)
+
+
+def health_check_database(request):
+    try:
+        connection.ensure_connection()
+        return HttpResponse("OK")
+    except Exception:
+        logger.exception("Database health check failed")
+        return HttpResponse("ERROR", status=500)
+
+
+def health_check_sftp(request):
+    try:
+        list_prisme_folder(settings.PRISME, settings.PRISME["control_folder"])
+        return HttpResponse("OK")
+    except ClientException:
+        logger.exception("SFTP health check failed")
+        return HttpResponse("ERROR", status=500)
