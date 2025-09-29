a2a605a41fb24f7cb7fb6b3a3e5ec6874d6ca4ed
Lars Peter Thomsen <larsp@magenta-aps.dk>
2025-09-29 13:52:44 +0200

    feat: [#66612] Update translations

--- suila/suila/models.py
+++ suila/suila/models.py
@@ -2231,7 +2231,7 @@ class EboksMessage(PermissionsMixin, models.Model):
     )
     recipient_status = models.CharField(
         choices=(
-            ("", _("Gyldig E-boks modtager")),
+            ("", _("Gyldig e-Boks modtager")),
             ("exempt", _("Fritaget modtager")),
             ("invalid", _("Ugyldig E-boks modtager (sendes til efterbehandling)")),
             ("dead", _("Afdød")),
