119f893518d29374cf86d5720561f480b12d4fc3
Mads Riis-Weitling <mrw@magenta.dk>
2026-08-18 17:27:27 +0200

    refactor: [#70373] Extract shared behavior to `BaseExport`
    `BatchExport` now implements the specific conversions, etc. for exporting
    `PersonMonth` querysets (i.e. "normal" Prisme benefit exports.)
    
    `BaseExport` contains shared/generic code.
    
    It remains to rename occurrences of `person_month`/`PersonMonth` in
    `BaseExport` to `obj`, etc.

--- suila/suila/integrations/prisme/final_settlements.py
+++ suila/suila/integrations/prisme/final_settlements.py
@@ -8,12 +8,13 @@ from dateutil.relativedelta import TU, relativedelta
 from django.db.models import CharField, QuerySet, Value
 from django.db.models.functions import Cast, LPad, Substr
 
-from suila.integrations.prisme.benefits import BatchExport
+from suila.integrations.prisme.benefits import BaseExport
 from suila.models import FinalSettlement, PrismeBatch, PrismeBatchItem
 
 
-class FinalSettlementExport(BatchExport):
+class FinalSettlementExport(BaseExport):
     def __init__(self, year: int):
+        assert year < date.today().year, f"`year` must be less than {date.today().year}"
         self._year = year
         self._month = date.today().month
 
@@ -39,12 +40,22 @@ class FinalSettlementExport(BatchExport):
         qs = qs.order_by("prefix", "annual_income__person_year__person__cpr")
         return qs
 
-    def get_prisme_account_alias_lookup(self, obj: FinalSettlement) -> tuple[str, int]:
+    def get_prisme_account_alias_lookup(
+        self, obj: FinalSettlement
+    ) -> tuple[str | None, int]:
         location_code: str | None = obj.annual_income.person_year.person.location_code
         tax_year: int = self._year
         return location_code, tax_year
 
-    def get_payment_amount(self, obj: FinalSettlement) -> Decimal:
+    def get_posting_text(self, obj: FinalSettlement) -> str:
+        cpr: str = obj.identifier  # type: ignore[attr-defined]
+        date_formatted: str = self.get_posting_date(obj).strftime("%b%y").upper()
+        return f"SUILA-TAPIT-{cpr}-{date_formatted}"
+
+    def get_transaction_text(self, obj: FinalSettlement) -> str:
+        return f"Suila.gl - Årsopgørelse {self._year}"
+
+    def get_payment_amount(self, obj: FinalSettlement) -> Decimal | None:
         return obj._result
 
     def get_payment_date(self, obj: FinalSettlement) -> date:
@@ -53,8 +64,17 @@ class FinalSettlementExport(BatchExport):
     def get_posting_date(self, obj: FinalSettlement) -> date:
         return date(self._year, self._month, 1) + relativedelta(weekday=TU(+2))
 
-    def get_posting_text(self, obj: FinalSettlement) -> str:
-        return f"Suila.gl - Årsopgørelse {self._year}"
+    def get_destination_filename(self, prisme_batch: PrismeBatch) -> str:
+        return (
+            "SUILA_aarsopgoerelse_G68_export_"
+            f"{prisme_batch.prefix:02}_{self._year}_{self._month:02}.g68"
+        )
+
+    def get_control_list_data(self) -> QuerySet:
+        return PrismeBatchItem.objects.none()  # TODO: implement
+
+    def get_control_list_filename(self) -> str:
+        return f"SUILA_kontrolliste_aarsopgoerelse_{self._year}_{self._month:02}.csv"
 
     def _get_prisme_batch_item_instance(
         self,
