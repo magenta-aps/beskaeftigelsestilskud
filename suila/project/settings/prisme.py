01d82c79ec1f31c1e7e4e894cac5fb45f5ff6d3f
Lars Peter Thomsen <larsp@magenta.dk>
2026-09-07 10:56:21 +0200

    feat: [#70372] Kode til prismeintegration

--- suila/project/settings/prisme.py
+++ suila/project/settings/prisme.py
@@ -29,6 +29,27 @@ PRISME = {
     "mod11_separate_cprs": literal_eval(
         os.environ.get("PRISME_MOD11_SEPARATE_CPRS", "[]")
     ),
+    "mock": os.environ.get("PRISME_MOCK", False),
+    "wsdl": os.environ.get("PRISME_WSDL", ""),
+    "auth": {
+        "basic": {
+            "username": os.environ.get("PRISME_SOAP_USERNAME", ""),
+            "domain": os.environ.get("PRISME_SOAP_DOMAIN", ""),
+            "password": os.environ.get("PRISME_SOAP_PASSWORD", ""),
+        }
+    },
+    "proxy": {"socks": os.environ.get("PRISME_SOCKS", None)},
+    "currency_code": os.environ.get("PRISME_CURRENCY_CODE", "DKK"),
+    "department_recid": os.environ.get("PRISME_DEPARTMENT_RECID"),
+    "department_recid_ext": os.environ.get("PRISME_DEPARTMENT_RECID_EXT"),
+    "invoice_ean": os.environ.get("PRISME_INVOICE_EAN"),
+    "order_form_num": os.environ.get("PRISME_ORDER_FORM_NUM"),
+    "contact_person_id": os.environ.get("PRISME_CONTACT_PERSON_ID"),
+    "project_name": os.environ.get("PRISME_PROJECT_NAME", "Suila"),
+    "project_category_id": int(os.environ.get("PRISME_PROJECT_CATEGORY_ID", "1")),
+    "finance_law_id": os.environ.get("PRISME_FINANCE_LAW_ID", 0),
+    "purpose_id": os.environ.get("PRISME_PURPOSE_ID", 0),
+    "type_account_plan_id": os.environ.get("PRISME_TYPE_ACCOUNT_PLAN_ID", 0),
 }
 
 # Seconds to wait between retries when talking to Prisme over SFTP. Zero while
