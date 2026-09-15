bc46a81a36b3b72902f30f7be526d6a93054752c
Peter Juelsgaard <pj@magenta-aps.dk>
2026-09-15 15:21:56 +0200

    fix: [#70633] Don't offset benefit if benefit_this_month < 0

--- suila/suila/benefit.py
+++ suila/suila/benefit.py
@@ -193,9 +193,13 @@ def calculate_benefit(
 
     # Offset surplus benefit first
     df["offset_benefit_difference"] = df.loc[
-        df.benefit_difference > 0, ["benefit_this_month", "benefit_difference"]
+        (df.benefit_difference > 0) & (df.benefit_this_month >= 0),
+        ["benefit_this_month", "benefit_difference"]
     ].min(axis=1)
-    df.loc[df.benefit_difference > 0, "benefit_this_month"] -= df[
+    df.loc[
+        (df.benefit_difference > 0) & (df.benefit_this_month >= 0),
+        "benefit_this_month"
+    ] -= df[
         "offset_benefit_difference"
     ]
 
